145ab7a2b47b80d1417313a05b5598743101edbd
smp15 <smp15.programming@gmail.com>
2018-06-19 11:50:14 +0200

    Implement react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+
 import "./style.css";
 import "./layout.css";
 
@@ -7,16 +14,27 @@ import Header from "./components/Header";
 import Menu from "./components/Menu";
 import CourseSelect from "./pages/CourseSelect";
 
+
 class App extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      currentPage: "courseSelect",
       user: {
         email: "abc@email.com",
-      }
+      },
+      pages: [
+        {
+          id: "module-keuze",
+          title: "Module keuze",
+        },
+        {
+          id: "instellingen",
+          title: "Instellingen",
+          bottom: true,
+        }
+      ],
     };
   }
 
@@ -25,22 +43,19 @@ class App extends Component {
     this.setState({ email: event.target.elements[0].value });
   }
 
-  getCurrentPage() {
-    switch (this.state.currentPage) {
-      case "courseSelect":
-        return <CourseSelect />;
-      case "login":
-      return <Login />;
-    }
-  }
-
   render() {
     return (
-      <div className="App" style={{ backgroundColor: "white" }}>
-        <Header email={this.state.user.email} />
-        <Menu/>
-        {this.getCurrentPage()}
-      </div>
+      <Router>
+        <div className="App" style={{ backgroundColor: "white" }}>
+          <Header email={this.state.user.email} />
+          <Menu pages={this.state.pages} />
+          <Switch>
+            <Route path="/course-select" component={CourseSelect} />
+            <Redirect to="/course-select" />
+          </Switch>
+        </div>
+      </Router>
+
     );
   }
 
--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -4,45 +4,56 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
 import Paper from '@material-ui/core/Paper';
 import Badge from '@material-ui/core/Badge';
+import { Redirect, withRouter } from 'react-router-dom';
 
 class Menu extends Component {
-    render() {
-        let Inbox;
-        if (this.props.notifications === 0) {
-            Inbox = (
-                <InboxIcon />
-            );
-        }else {
-            Inbox = (
-                <Badge badgeContent={this.props.notifications} color="primary">
-                    <InboxIcon />
-                </Badge>
-            )
-        }
 
+    onClick(page) {
+        this.props.history.push("/" + page);
+    }
+
+    render() {
+        var pages = this.props.pages.map((page) => {
+
+            let style = {};
+            if (page.bottom) {
+                style.position = "absolute";
+                style.bottom = "0px";
+            }
+
+            if (page.notifications > 0) {
+                return (
+                    <ListItem button onClick={() => this.onClick(page.id)} style={style}>
+                        <ListItemIcon>
+                            <Badge badgeContent={this.props.notifications} color="primary">
+                                <InboxIcon />
+                            </Badge>
+                        </ListItemIcon>
+                        <ListItemText primary={page.title} />
+                    </ListItem>
+                );
+            } else {
+                return (
+                    <ListItem button onClick={() => this.onClick(page.id)} style={style}>
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={page.title} />
+                    </ListItem>
+                );
+            }
+        });
         return (
             <Paper elevation={8} className="Menu">
-                <List component="nav">
-                    <ListItem button>
-                        <ListItemIcon>
-                            {Inbox}
-                        </ListItemIcon>
-                        <ListItemText primary="Inbox" />
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemIcon>
-                            <DraftsIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Drafts" />
-                    </ListItem>
+                <List component="nav" style={{ height: "100%" }}>
+                    {pages}
                 </List>
-            </Paper>
+            </Paper >
         );
     }
 }
 
-export default Menu;
+export default withRouter(Menu);
 
