679f59f9c63e62802cec1a6a83df3c0a70d03cbb
smp15 <smp15.programming@gmail.com>
2018-06-19 18:51:06 +0200

    Hook-up course endpoints

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,59 +1,34 @@
 var express = require("express");
 var router = express.Router();
+var Database = require('../database');
 
-var courses = [{
-  "key": "0",
-  "name": "Informatica",
-  "subject": null,
-  "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-  "teacher": null,
-  "period": 1,
-  "schoolyear": "2018/2019",
-}, {
-  "key": "1",
-  "name": "Filosofie",
-  "subject": null,
-  "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-  "teacher": null,
-  "period": 1,
-  "schoolyear": "2018/2019",
-}, {
-  "key": "2",
-  "subject": null,
-  "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-  "name": "Spaans",
-  "teacher": null,
-  "period": 1,
-  "schoolyear": "2018/2019",
-}, {
-  "key": "3",
-  "name": "Tekenen",
-  "subject": null,
-  "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-  "teacher": null,
-  "period": 1,
-  "schoolyear": "2018/2019",
-}, {
-  "key": "4",
-  "name": "Spaans per2",
-  "subject": null,
-  "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-  "teacher": null,
-  "period": 2,
-  "schoolyear": "2018/2019",
-}];
+
+function toClientCourse(databaseCourse) {
+  return {
+    key: databaseCourse.id,
+    subject: null,
+    name: databaseCourse.name,
+    description: databaseCourse.description,
+    teacher: null,
+    period: databaseCourse.period,
+    schoolyear: databaseCourse.schoolyear,
+  }
+}
 
 /* GET users listing. */
 router.get("/list", function (req, res, next) {
   res.setHeader("Content-Type", "application/json");
-  res.send(courses);
+  Database.getCourses().then(courses => {
+    res.send(courses.map(toClientCourse));
+  });
 });
 
 router.get("/choices", function (req, res, next) {
   res.setHeader("Content-Type", "application/json");
-
-  var choices = courses.filter((course) => { return course.period == 1 });
-  res.send(choices);
+  Database.getCourses().then(courses => {
+    var choices = courses.filter((course) => { return course.period == 1 });
+    res.send(choices.map(toClientCourse));
+  });
 });
 
 module.exports = router;
