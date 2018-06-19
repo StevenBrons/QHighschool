4cb40f69c8be10facd4ed9a7c1a069eb40171418
smp15 <smp15.programming@gmail.com>
2018-06-19 18:49:48 +0200

    Add checkToken method and InvalidTokenError

--- server/database.js
+++ server/database.js
@@ -1,5 +1,15 @@
 const mysql = require('promise-mysql');
 
+class InvalidTokenError extends Error {
+  constructor(unset) {
+    if (unset === true) {
+      super("Token unset");
+    }else {
+      super("Invalid token");
+    }
+  }
+}
+
 class Database {
   async connect(connectionArgs) {
     this.connection = await mysql.createConnection(connectionArgs);
@@ -10,7 +20,16 @@ class Database {
       if (users.length == 1) {
         return users[0];
       } else {
-        return this.checkToken(token);
+  async checkToken(token) {
+    if (token == null || token == "") {
+      throw new InvalidTokenError(true);
+    }
+    return this.connection.query('SELECT id FROM loggedin WHERE token = ?', [token]).then((id) => {
+      if (id.length !== 1) {
+        throw new InvalidTokenError();
+      }
+    });
+  }
       }
     });
   }
