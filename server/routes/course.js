7779d0b7cf1c07c0b76cf5a3a31c0938c7fca9eb
smp15 <smp15.programming@gmail.com>
2018-06-19 14:28:50 +0200

    Add choices endpoint

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,45 +1,59 @@
 var express = require("express");
 var router = express.Router();
 
+var courses = [{
+  "key": "0",
+  "name": "Informatica",
+  "subject": null,
+  "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
+  "teacher": null,
+  "period": 1,
+  "schoolyear": "2018/2019",
+}, {
+  "key": "1",
+  "name": "Filosofie",
+  "subject": null,
+  "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
+  "teacher": null,
+  "period": 1,
+  "schoolyear": "2018/2019",
+}, {
+  "key": "2",
+  "subject": null,
+  "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
+  "name": "Spaans",
+  "teacher": null,
+  "period": 1,
+  "schoolyear": "2018/2019",
+}, {
+  "key": "3",
+  "name": "Tekenen",
+  "subject": null,
+  "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
+  "teacher": null,
+  "period": 1,
+  "schoolyear": "2018/2019",
+}, {
+  "key": "4",
+  "name": "Spaans per2",
+  "subject": null,
+  "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
+  "teacher": null,
+  "period": 2,
+  "schoolyear": "2018/2019",
+}];
+
 /* GET users listing. */
 router.get("/list", function (req, res, next) {
-
-  var courses = [{
-    "key": "0",
-    "name": "Informatica",
-    "subject": null,
-    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-    "teacher": null,
-    "period": 1,
-    "schoolyear": "2018/2019",
-  }, {
-    "key": "1",
-    "name": "Filosofie",
-    "subject": null,
-    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-    "teacher": null,
-    "period": 1,
-    "schoolyear": "2018/2019",
-  }, {
-    "key": "2",
-    "subject": null,
-    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-    "name": "Spaans",
-    "teacher": null,
-    "period": 1,
-    "schoolyear": "2018/2019",
-  }, {
-    "key": "3",
-    "name": "Tekenen",
-    "subject": null,
-    "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. ",
-    "teacher": null,
-    "period": 1,
-    "schoolyear": "2018/2019",
-  }];
-
   res.setHeader("Content-Type", "application/json");
   res.send(courses);
+});
+
+router.get("/choices", function (req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+
+  var choices = courses.filter((course) => { return course.period == 1 });
+  res.send(choices);
 });
 
 module.exports = router;
