f68952339b4ac54a3ed709c63c61d6c326f2eb8c
smp15 <smp15.programming@gmail.com>
2018-06-19 18:48:09 +0200

    Add getUser method

--- server/database.js
+++ server/database.js
@@ -5,6 +5,15 @@ class Database {
     this.connection = await mysql.createConnection(connectionArgs);
   }
 
+  async getUser(token) {
+    return this.connection.query('SELECT * FROM user WHERE id IN (SELECT id FROM loggedin WHERE token = ?);', [token]).then((users) => {
+      if (users.length == 1) {
+        return users[0];
+      } else {
+        return this.checkToken(token);
+      }
+    });
+  }
 }
 
 module.exports = Database;
