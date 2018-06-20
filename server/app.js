39b07c2fa28952cdac926ecb389e464d0cfc1954
smp15 <smp15.programming@gmail.com>
2018-06-20 11:51:24 +0200

    Add CORS

--- server/app.js
+++ server/app.js
@@ -29,9 +29,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader('Access-Control-Allow-Origin', '*');//a webadres
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
 app.use('/api/course', courseRoute);
 app.use('/api/user', userRoute);
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
