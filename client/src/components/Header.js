3db775448196c3e84543d4d0e033d8b5418f97d8
smp15 <smp15.programming@gmail.com>
2018-06-21 14:57:07 +0200

    Add profile menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
 class Header extends Component {
-    render() {
-        return (
-            <AppBar className="Header">
-                <Toolbar>
-                    <IconButton color="inherit" aria-label="Menu" onClick={this.props.handleShowMenu}>
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="!title" color="inherit">
-                        Q-Highschool
-                    </Typography>
-                    <Button color="inherit" style={{right:10,position:"absolute"}}>{this.props.email}</Button>
-                </Toolbar>
-            </AppBar>
-        );
-    }
+
+	constructor(props) {
+		super(props);
+		this.state = { anchorEl: null };
+	}
+
+	handleClick = event => {
+		this.setState({ anchorEl: event.currentTarget });
+	};
+
+	handleClose = () => {
+		this.setState({ anchorEl: null });
+	};
+
+	render() {
+		const { anchorEl } = this.state;
+
+		return (
+			<AppBar className="Header">
+				<Toolbar>
+					<IconButton color="inherit" aria-label="Menu" onClick={this.props.handleShowMenu}>
+						<MenuIcon />
+					</IconButton>
+						Q-Highschool
+					<Button color="inherit" style={{ right: 10, position: "absolute" }} onClick={this.handleClick}>{this.props.email}</Button>
+				</Toolbar>
+				<Menu
+					id="simple-menu"
+					anchorEl={anchorEl}
+					open={Boolean(anchorEl)}
+					onClose={this.handleClose}
+				>
+					<MenuItem onClick={this.handleClose}>Profiel</MenuItem>
+					<MenuItem onClick={this.handleClose}>Log uit</MenuItem>
+				</Menu>
+			</AppBar>
+		);
+	}
 }
 
 export default Header;
