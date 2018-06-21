c4410406e51fcaefeef93b0df269c7ec37e94512
smp15 <smp15.programming@gmail.com>
2018-06-21 16:00:28 +0200

    Start post endpoints

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,64 +3,69 @@ var router = express.Router();
 var Database = require('../database');
 
 function toClientUser(databaseUser) {
-    return {
-        key: databaseUser.id,
-        role: databaseUser.role,
-        school: databaseUser.school,
-        firstname: databaseUser.firstname,
-        lastname: databaseUser.lastname,
-        year: databaseUser.year,
-        level: databaseUser.level,
-        preferedEmail: databaseUser.preferedemail
-    }
+	return {
+		key: databaseUser.id,
+		role: databaseUser.role,
+		school: databaseUser.school,
+		firstname: databaseUser.firstname,
+		lastname: databaseUser.lastname,
+		year: databaseUser.year,
+		level: databaseUser.level,
+		preferedEmail: databaseUser.preferedemail
+	}
 }
 
 function toClientChoice(databaseChoice) {
-    if (databaseChoice !== null && databaseChoice !== undefined) {
-        var choices = [];
-        choices[0] = databaseChoice.firstchoice
-        choices[1] = databaseChoice.secondchoice
-        choices[2] = databaseChoice.thirdchoice
-        return choices;
-    } else {
-        return [];
-    }
+	if (databaseChoice !== null && databaseChoice !== undefined) {
+		var choices = [];
+		choices[0] = databaseChoice.firstchoice
+		choices[1] = databaseChoice.secondchoice
+		choices[2] = databaseChoice.thirdchoice
+		return choices;
+	} else {
+		return [];
+	}
 }
 
 function handleError(error, res) {
-    res.send({
-        error: error.message,
-    });
+	res.send({
+		error: error.message,
+	});
 }
 
-var choices = [
-
-]
-
 router.get("/", (req, res) => {
-    Database.getUser(req.headers.userid).then((user) => {
-        res.send(toClientUser(user));
-    }).catch(error => {
-        handleError(error, res);
-    });
+	Database.getUser(req.headers.userid).then((user) => {
+		res.send(toClientUser(user));
+	}).catch(error => {
+		handleError(error, res);
+	});
 });
 
 router.post("/", (req, res) => {
-    user.preferedEmail = req.body.preferedEmail
-    res.send(user);
+	Database.setUser(req.headers.userid, req.body).then((data) => {
+		res.send({
+			success: true,
+		});
+	});
 });
 
 router.post("/choices", (req, res) => {
-    choices = (req.body.choices).split(",");
-    res.send(choices);
+	let choices = (req.body.choices).split(",");
+	Database.setUserChoices(req.headers.userid, choices).then((data) => {
+		res.send({
+			success: true,
+		});
+	}).catch((error) => {
+		handleError(error, res);
+	});
 });
 
 router.get("/choices", (req, res) => {
-    Database.getChoice(req.headers.userid).then(choice => {
-        res.send(toClientChoice(choice));
-    }).catch((error) => {
-        handleError(error, res);
-    });
+	Database.getChoice(req.headers.userid).then(choice => {
+		res.send(toClientChoice(choice));
+	}).catch((error) => {
+		handleError(error, res);
+	});
 });
 
 module.exports = router;
