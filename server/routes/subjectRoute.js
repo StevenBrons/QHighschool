3fd0e2f619990b3ea1f732029c43a0a945355c61
smp15 <smp15.programming@gmail.com>
2018-06-23 16:09:23 +0200

    Namespacing

--- server/routes/subjectRoute.js
+++ server/routes/subjectRoute.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var database = require('../database/MainDB');
+var database = require('../database/mainDb');
 
 router.get("/list", function (req, res, next) {
   database.subject.getSubjects().then(subjects => {
