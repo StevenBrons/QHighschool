622af54979d5ee622e6e93c4cd7ef7f115f597e7
smp15 <smp15.programming@gmail.com>
2018-06-23 13:12:55 +0200

    Refactor database class

---
+++ server/database/MainDB.js
@@ -0,0 +1,46 @@
+const mysql = require("promise-mysql");
+const UserDB = require("./UserDB");
+const SubjectDB = require("./SubjectDB");
+const CourseDB = require("./CourseDB");
+
+class InvalidTokenError extends Error {
+	constructor(unset) {
+		if (unset === true) {
+			super("Token unset");
+		} else {
+			super("Invalid token");
+		}
+	}
+}
+
+class Database {
+
+	constructor() {
+		this.connect(require('../private/keys').databaseArgs);
+	}
+
+	async connect(connectionArgs) {
+		this.connection = await mysql.createConnection(connectionArgs);
+		this.course = new CourseDB(this);
+		this.subject = new SubjectDB(this);
+		this.user = new UserDB(this);
+	}
+
+	async checkToken(token) {
+		if (token == null || token == "") {
+			return Promise.reject(new InvalidTokenError(true));
+		}
+		return this.connection.query(
+			"SELECT id FROM loggedin " +
+			"WHERE token = ?",
+			[token]).then((id) => {
+				if (id.length !== 1) {
+					throw new InvalidTokenError();
+				}
+			});
+	}
+
+}
+
+
+module.exports = new Database();
