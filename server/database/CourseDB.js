e03cb04aee0306e7c232fda94efd151a40e08ec0
smp15 <smp15.programming@gmail.com>
2018-06-25 17:17:49 +0200

    Update getCourse endpoint and documentation

--- server/database/CourseDB.js
+++ server/database/CourseDB.js
@@ -5,12 +5,27 @@ class CourseDB {
 	}
 
 	async getCourses() {
-		return this.mainDb.connection.query("SELECT * FROM course;");
+		return this.mainDb.connection.query(				
+			"SELECT "  +
+			"course.* ," +
+			"subject.name AS subjectName," +
+			"concat(user.firstName, ' ', user.lastName) AS teacherName " +
+			"FROM course INNER JOIN user ON course.teacherId = user.id " +
+			"INNER JOIN subject ON course.subjectId = subject.id "
+		);
 	}
 
 	async getCourse(body) {
 		if (body.courseId >= 0) {
-			return this.mainDb.connection.query("SELECT * FROM course WHERE id = ?", [body.courseId]).then(courses => {
+			return this.mainDb.connection.query(
+				"SELECT "  +
+				"course.* ," +
+				"subject.name AS subjectName," +
+				"concat(user.firstName, ' ', user.lastName) AS teacherName " +
+				"FROM course INNER JOIN user ON course.teacherId = user.id " +
+				"INNER JOIN subject ON course.subjectId = subject.id " +
+				"WHERE course.id = ?;"
+			, [body.courseId]).then(courses => {
 				if (courses.length === 1) {
 					return courses[0];
 				}
