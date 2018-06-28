362cf493a2492cce23171db14b482d7d11d5a663
smp15 <smp15.programming@gmail.com>
2018-06-28 13:55:58 +0200

    Create global provider class

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import {
-	BrowserRouter as Router,
 	Route,
 	Switch,
 	Redirect,
+	withRouter,
 } from 'react-router-dom';
 import Data, { User } from "./Data";
 import {getCookie,setCookie} from "./lib/Cookie";
@@ -15,25 +15,6 @@ import Course from "./pages/Course";
 
 import Header from "./components/Header";
 import Menu from "./components/Menu";
-
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-
-const theme = createMuiTheme({
-	palette: {
-		primary: {
-			light: '#5472d3',
-			main: '#0d47a1',
-			dark: '#002171',
-			contrastText: '#fff',
-		},
-		secondary: {
-			light: '#60ad5e',
-			main: '#2e7d32',
-			dark: '#005005',
-			contrastText: '#ffffff',
-		},
-	},
-});
 
 class App extends Component {
 
@@ -100,24 +81,18 @@ class App extends Component {
 			);
 		}
 		return (
-			<MuiThemeProvider theme={theme}>
-				<Router>
 					<div className="App" style={{ backgroundColor: "white" }}>
-						{
-							this.state.showMenu ? <Menu pages={this.state.pages} /> : null
-						}
+				{this.props.showMenu && <Menu pages={this.state.pages} />}
 						<Header email={this.state.user.preferedEmail} handleShowMenu={this.handleShowMenu.bind(this)} path={this.props.location} />
 						<Switch>
+					<Route path="/login" component={Login} />
+					{!this.props.isLoggedIn && <Redirect to="/login" />}
 							<Route path="/module-keuze" component={CourseSelect} />
 							<Route path="/module" component={Course} />
-							<Route path="/instellingen" render={() => {
-								return (<Settings onSave={this.onSettingsSave.bind(this)} user={this.state.user}/>);
-							}} />
-							<Redirect to="/module-keuze" />
+					<Route path="/instellingen" component={Settings} />
+					<Redirect push to="/module-keuze" />
 						</Switch>
 					</div>
-				</Router>
-			</MuiThemeProvider>
 		);
 	}
 
