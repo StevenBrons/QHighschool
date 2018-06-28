576fdf7139aa6d55a6798e655995095de4be327f
smp15 <smp15.programming@gmail.com>
2018-06-28 13:52:49 +0200

    Add Redux and Thunk to application

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore, applyMiddleware, compose as reduxCompose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducer from './store/reducer';
+import GlobalProvider from './GlobalProvider';
 import "./layout.css";
 import 'typeface-roboto'
 
+const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || reduxCompose;
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(thunk),
+  ),
+);
+
 ReactDOM.render(
-    <App />
-    , document.getElementById('root'));
+	<GlobalProvider store={store} />,
+	document.getElementById('root'));
