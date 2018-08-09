3f330a749b9afd8774be54ac651f080d9575eef7
smp15 <smp15.programming@gmail.com>
2018-08-09 10:37:40 +0200

    Remove redundant code

--- server/database/MainDB.js
+++ server/database/MainDB.js
@@ -5,23 +5,6 @@ const CourseDB = require("./CourseDB");
 const GroupDB = require("./GroupDB");
 const SessionDB = require("./SessionDB");
 
-class InvalidTokenError extends Error {
-	constructor(unset) {
-		if (unset === true) {
-			super("Token unset");
-		} else {
-			super("Invalid token");
-		}
-	}
-}
-
-class InsufficientPrivileges extends Error {
-	constructor() {
-		super("You have insufficient privileges to execute this action");
-	}
-}
-
-
 class Database {
 
 	constructor() {
@@ -37,26 +20,6 @@ class Database {
 		this.user = new UserDB(this);
 		this.group = new GroupDB(this);
 		this.session = new SessionDB(this);
-	}
-
-	async checkToken(token, allowedRoles) {
-		if (token == null || token == "") {
-			return Promise.reject(new InvalidTokenError(true));
-		}
-		return this.connection.query(
-			"SELECT user_data.role FROM loggedin " +
-			"INNER JOIN user_data ON user_data.id = loggedin.id " +
-			"WHERE loggedin.token = ?" ,
-			[token]).then((items) => {
-				if (items.length === 1) {
-					if (allowedRoles.includes(items[0].role)) {
-						return items[0].role;
-					}else {
-						throw new InsufficientPrivileges();
-					}
-				}
-				throw new InvalidTokenError();
-			});
 	}
 
 }
