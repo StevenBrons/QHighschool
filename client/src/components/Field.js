ef8ee1e45b926ea9420f25a55aa98e365afaa329
StevenBrons <stevengoud@gmail.com>
2018-08-30 12:51:29 +0200

    Improve field component

--- client/src/components/Field.js
+++ client/src/components/Field.js
@@ -11,42 +11,42 @@ class Field extends Component {
 			error: false,
 		}
 	}
-	
+
 	componentDidMount() {
 		this.checkRequirements(this.props.value);
 	}
 
 	checkRequirements(value) {
 		let error = false;
-		if (this.props.email){
+		if (this.props.email) {
 			const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 			if (!re.test(value)) {
 				error = true;
 			}
 		}
-		if (this.props.integer){
+		if (this.props.integer) {
 			const re = /^\+?[1-9][\d]*$/i;
 			if (!re.test(value)) {
 				error = true;
 			}
 		}
-		if (this.props.min){
-			if (parseInt(value,10) < this.props.min) {
+		if (this.props.min) {
+			if (parseInt(value, 10) < this.props.min) {
 				error = true;
 			}
 		}
-		if (this.props.max){
-			if (parseInt(value,10) > this.props.max) {
+		if (this.props.max) {
+			if (parseInt(value, 10) > this.props.max) {
 				error = true;
 			}
 		}
-		if (this.props.phoneNumber){
+		if (this.props.phoneNumber) {
 			const re = /(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)/i;
 			if (!re.test(value)) {
 				error = true;
 			}
 		}
-		if (this.props.notEmpty){
+		if (this.props.notEmpty) {
 			if (value == null || value === "" || value === " " || (this.props.options != null && this.props.options.indexOf(value) === -1)) {
 				error = true;
 			}
@@ -61,12 +61,12 @@ class Field extends Component {
 		this.checkRequirements(event.target.value);
 		this.props.onChange({
 			...event,
-			name:this.props.name,
+			name: this.props.name,
 		});
 	}
 
 	render() {
-		let value = this.props.value == null?"":this.props.value;
+		let value = this.props.value == null ? "" : this.props.value;
 		let textAlign = this.props.right ? "right" : "left";
 		let float = this.props.right ? "right" : "none";
 		let color = theme.palette.text.primary;
@@ -75,19 +75,11 @@ class Field extends Component {
 		let fullWidth = false;
 		let style = this.props.style;
 		let disabled = true;
-		let disableUnderline = this.props.disableUnderline||true;
-		let margin = this.props.margin||"none";
+		let options = this.props.options;
+		let disableUnderline = this.props.disableUnderline || true;
+		let margin = this.props.margin || "none";
 		let multiline = false;
 		let menuItems;
-		if (this.props.options) {
-			menuItems = this.props.options.map(option => {
-				return (
-					<MenuItem key={option} value={option}>
-						{option}
-					</MenuItem>
-				)
-			});
-		}
 		if (style == null) {
 			style = {};
 		}
@@ -104,14 +96,37 @@ class Field extends Component {
 		}
 		if (this.props.editable) {
 			disabled = false;
-			disableUnderline = this.props.disableUnderline||false;
-			margin = this.props.margin||"normal";
+			disableUnderline = this.props.disableUnderline || false;
+			margin = this.props.margin || "normal";
+		} else {
+			if (typeof options === "object") {
+				value = options.filter((opt) => {
+					return opt.value === value;
+				})[0].label;
+			}
+			options = null;
 		}
 		if (this.props.title) {
 			color = theme.palette.primary.main;
 			style.lineHeight = "1.16667em";
 			fontSize = "1.3125rem";
 			fontWeight = "500";
+		}
+		if (options) {
+			menuItems = options.map(option => {
+				if (typeof option === "object") {
+					return (
+						<MenuItem key={option.value} value={option.value}>
+							{option.label}
+						</MenuItem>
+					)
+				}
+				return (
+					<MenuItem key={option} value={option}>
+						{option}
+					</MenuItem>
+				)
+			});
 		}
 
 		return (
@@ -121,9 +136,9 @@ class Field extends Component {
 				disabled={disabled}
 				fullWidth={fullWidth}
 				multiline={multiline}
-				className={this.props.right?"right":""}
+				className={this.props.right ? "right" : ""}
 				label={this.props.label}
-				select={this.props.options ? true : false}
+				select={options ? true : false}
 				style={{ float, flex: 1, marginLeft: "10px", marginRight: "10px" }}
 				onChange={this.onChange.bind(this)}
 				error={this.state.error}
