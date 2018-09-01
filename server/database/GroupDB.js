f215ff87c766f090f9765d2fc93c1ecbada1f5e4
smp15 <smp15.programming@gmail.com>
2018-09-01 13:39:13 +0200

    Remove teacherId as explicit database field

--- server/database/GroupDB.js
+++ server/database/GroupDB.js
@@ -5,40 +5,72 @@ class GroupDB {
 
 	async getGroups() {
 		return this.mainDb.connection.query(
-			"SELECT course_group.*, " +
-			"course.name AS courseName, " +
-			"course.description AS courseDescription, " +
-			"course.foreknowledge AS foreknowledge, " +
-			"course.studyTime AS studyTime, " +
-			"school_subject.id AS subjectId, " +
-			"school_subject.name AS subjectName, " +
-			"school_subject.description AS subjectDescription, " +
-			"CONCAT(user_data.firstName, ' ', user_data.lastName) AS teacherName " +
-			"FROM course_group " +
-			"INNER JOIN course ON course.id = course_group.courseId " +
-			"INNER JOIN user_data ON user_data.id = course_group.teacherId " +
-			"INNER JOIN school_subject ON school_subject.id = course.subjectId " +
-			"ORDER BY course_group.period"
+			"			SELECT  " +
+			"			course_group.*, " +
+			"			course.name AS courseName, " +
+			"			course.description AS courseDescription, " +
+			"			course.foreknowledge AS foreknowledge, " +
+			"			course.studyTime AS studyTime, " +
+			"			school_subject.id AS subjectId, " +
+			"			school_subject.name AS subjectName, " +
+			"			school_subject.description AS subjectDescription, " +
+			"			(SELECT  " +
+			"							userId " +
+			"					FROM " +
+			"							participant " +
+			"					WHERE " +
+			"							groupId = course_group.id " +
+			"									AND `status` = 'teacher' " +
+			"					LIMIT 1) AS teacherId, " +
+			"			(SELECT  " +
+			"							displayName " +
+			"					FROM " +
+			"							user_data " +
+			"					WHERE " +
+			"							id = teacherId) AS teacherName " +
+			"	FROM " +
+			"			course_group " +
+			"					INNER JOIN " +
+			"			course ON course.id = course_group.courseId " +
+			"					INNER JOIN " +
+			"			school_subject ON school_subject.id = course.subjectId " +
+			"	ORDER BY course_group.period "
 		);
 	}
 
 	async getGroup(groupId) {
 		if (groupId >= 0) {
 			return this.mainDb.connection.query(
-				"SELECT course_group.*, " +
-				"course.name AS courseName, " +
-				"course.description AS courseDescription, " +
-				"course.foreknowledge AS foreknowledge, " +
-				"course.studyTime AS studyTime, " +
-				"school_subject.name AS subjectName, " +
-				"school_subject.id AS subjectId, " +
-				"school_subject.description AS subjectDescription, " +
-				"CONCAT(user_data.firstName, ' ', user_data.lastName) AS teacherName " +
-				"FROM course_group " +
-				"INNER JOIN course ON course.id = course_group.courseId " +
-				"INNER JOIN user_data ON user_data.id = course_group.teacherId " +
-				"INNER JOIN school_subject ON school_subject.id = course.subjectId " +
-				"WHERE course_group.id = ? ",
+				"			SELECT  " +
+				"			course_group.*, " +
+				"			course.name AS courseName, " +
+				"			course.description AS courseDescription, " +
+				"			course.foreknowledge AS foreknowledge, " +
+				"			course.studyTime AS studyTime, " +
+				"			school_subject.id AS subjectId, " +
+				"			school_subject.name AS subjectName, " +
+				"			school_subject.description AS subjectDescription, " +
+				"			(SELECT  " +
+				"							userId " +
+				"					FROM " +
+				"							participant " +
+				"					WHERE " +
+				"							groupId = course_group.id " +
+				"									AND `status` = 'teacher' " +
+				"					LIMIT 1) AS teacherId, " +
+				"			(SELECT  " +
+				"							displayName " +
+				"					FROM " +
+				"							user_data " +
+				"					WHERE " +
+				"							id = teacherId) AS teacherName " +
+				"	FROM " +
+				"			course_group " +
+				"					INNER JOIN " +
+				"			course ON course.id = course_group.courseId " +
+				"					INNER JOIN " +
+				"			school_subject ON school_subject.id = course.subjectId " +
+				" WHERE course_group.id = ? ",
 				[groupId]).then(groups => {
 					if (groups.length === 1) {
 						return groups[0];
