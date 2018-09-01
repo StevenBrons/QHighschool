85c73c485597a25c14eda16def0800f4c0fbdb9f
smp15 <smp15.programming@gmail.com>
2018-09-01 13:09:07 +0200

    Improve lesson layout

--- client/src/components/Lesson.js
+++ client/src/components/Lesson.js
@@ -48,13 +48,13 @@ class Lesson extends Component {
 						variant="dense"
 						disableGutters
 					>
-						<div style={{ flex: "10px" }}>
+						<div style={{ flex: 1 }}>
 							<Field style={{ fontWeight: "bold" }} value="Lesnummer" />
 							<Field style={{ fontWeight: "bold" }} value="Soort les" />
 						</div>
-						<Field style={{ flex: 2, fontWeight: "bold" }} value="Onderwerp" />
-						<Field style={{ flex: 4, fontWeight: "bold" }} value="Activiteiten" />
-						<div style={{ flex: "20px" }}>
+						<Field style={{ flex: 4, fontWeight: "bold" }} value="Onderwerp" />
+						<Field style={{ flex: 8, fontWeight: "bold" }} value="Activiteiten" />
+						<div style={{ flex: 2 }}>
 							<Field style={{ fontWeight: "bold" }} value="Datum" />
 							<Field style={{ fontWeight: "bold" }} value="Aanwezigheid" />
 						</div>
@@ -69,13 +69,13 @@ class Lesson extends Component {
 				onMouseLeave={() => this.setState({ hover: false })}
 				style={this.state.style}
 			>
-				<div style={{ flex: "10px" }}>
+				<div style={{ flex: 1 }}>
 					<Field margin="none" variant="body1" color="primary" style={{ fontWeight: "bold" }} value={"Les " + lesson.numberInBlock} />
 					<Field margin="none" label="Soort les" name="kind" variant="body1" value={lesson.kind} editable={this.props.editable} onChange={this.handleChange} />
 				</div>
-				<Field margin="none" label="Onderwerp" area name="subject" variant="body1" style={{ flex: 2 }} value={lesson.subject} editable={this.props.editable} onChange={this.handleChange} />
-				<Field margin="none" label="Activiteiten" area name="activities" variant="body1" style={{ flex: 4 }} value={lesson.activities} editable={this.props.editable} onChange={this.handleChange} />
-				<div style={{ flex: "20px" }}>
+				<Field margin="none" label="Onderwerp" area name="subject" variant="body1" style={{ flex: 4 }} value={lesson.subject} editable={this.props.editable} onChange={this.handleChange} />
+				<Field margin="none" label="Activiteiten" area name="activities" variant="body1" style={{ flex: 8 }} value={lesson.activities} editable={this.props.editable} onChange={this.handleChange} />
+				<div style={{ flex: 2 }}>
 					<Field margin="none" variant="body1" value={this.getWeekdayString(new Date(lesson.date).getDay()) + " " + new Date(lesson.date).getDate() + "-" + (new Date(lesson.date).getMonth() + 1) + "-" + new Date(lesson.date).getFullYear()} />
 					<Field margin="none" name="presence" label="Aanwezigheid" labelVisible variant="body1" value={lesson.presence} editable={this.props.editable} onChange={this.handleChange} options={[{label:"Noodzakelijk",value:"required"},{label:"Optioneel",value:"optional"},{label:"Niet nodig",value:"unrequired"}]}/>
 				</div>
