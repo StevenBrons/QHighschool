c990d421310954afebec9f3b8409b23114d2ce14
unknown <jorritdb@hotmail.com>
2019-02-14 17:53:09 +0100

    Table now displays data

--- client/src/pages/DataPage.js
+++ client/src/pages/DataPage.js
@@ -3,8 +3,7 @@ import Page from "./Page";
 import Progress from '../components/Progress';
 
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import { Paper, Typography, Button, Table, TableHead, TableCell, TableBody, TableRow } from "@material-ui/core";
+import { Paper, Typography, Table, TableHead, TableCell, TableBody, TableRow } from "@material-ui/core";
 import Field from '../components/Field';
 import queryString from "query-string";
 import Toolbar from '@material-ui/core/Toolbar';
@@ -39,26 +38,37 @@ class DataPage extends Component {
 		const dropDownOption = "user_data"; // or "evaluations", or "enrollments"
 		//</TEMP>
 
-		//use this function to fetch the data
-		// if (this.state.data == null) {
-		// 	this.props.fetchData(dropDownOption).then(data => this.setState({ data: data }));
-		// }
-		let data = [["displayName", "firstName", "lastName", "role"],
-		["B", "Steven", "Steven", "B", "admin"],
-		["Doe","Jon", "Jon", "Doe", "student"],
-		["B", "Steven", "Steven", "B", "admin"],
-		["Doe", "Jon", "Jon", "Doe", "student"],
-		["B", "Steven", "Steven", "B", "admin"],
-		["Doe", "Jon", "Jon", "Doe", "student"],
-		["B", "Steven", "Steven", "B", "admin"],
-		["Doe", "Jon", "Jon", "Doe", "student"]];
-		// placeholder so I don't have to deal with async data
-		console.log(this.state.data)
-		// <Typography variant = "subheading" color="textSecondary" style = {{flex: "1 1 auto"}}>
-		// 					Gegevens
-		// </Typography>
-		console.log(data[0].length);
-
+		let content;
+		if (this.state.data == null) {
+			content = <Progress/>
+			this.props.fetchData(dropDownOption).then(data => this.setState({ data: data }));
+		} else {
+			content = <Table>
+						<TableHead>
+							<TableRow>
+								{this.state.data[0].map((title) => {
+									return(
+										<TableCell>{title}</TableCell>
+									)
+								})}
+							</TableRow>
+						</TableHead> 
+						<TableBody>
+							{this.state.data.filter((_,index) => { return(index > 0); } )//take everything but the header
+								.map((row, index) => {
+									return(
+										<TableRow key={index} >
+											{row.map((cell) => {
+												return(
+													<TableCell>{cell}</TableCell>
+												)
+											})}
+										</TableRow>
+									)
+							})} 
+						</TableBody>
+					</Table>
+		}
 		return (
 			<Page>
 				<Paper elevation = {2} style= {{position: "relative"}}> 
@@ -80,28 +90,11 @@ class DataPage extends Component {
 					</Toolbar>
 				</Paper>
 				<br />
+				<div>
 					<Table>
-						<TableHead >
-							<TableRow>
-								<TableCell allign ="right">Piet</TableCell>
-								<TableCell allign="right">Jan</TableCell>
-								<TableCell backgroundcolor = "black" >Klaas</TableCell>
-								<TableCell>Henk</TableCell>
-								<TableCell>Pieter</TableCell> 
-							</TableRow>
-						</TableHead>	
-					<TableBody>
-						{data.map(row => (
-							<TableRow>
-								<TableCell>{row[0]}</TableCell>
-								<TableCell>{row[1]}</TableCell>
-								<TableCell>{row[2]}</TableCell>
-								<TableCell>{row[3]}</TableCell>
-								<TableCell>{row[4]}</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
+						{content}
 					</Table>
+				</div>
 			</Page>
 		);
 	}
@@ -110,6 +103,7 @@ class DataPage extends Component {
 
 function mapStateToProps(state) {
 	return {
+		data: state.data,
 	};
 }
 
@@ -122,13 +116,13 @@ function mapDispatchToProps(dispatch) {
 					return [
 						["displayName", "firstName", "lastName", "role"],
 						["B, Steven", "Steven", "B", "admin"],
-						["Doe, Jon", "Jon", "Doe", "student"]
-						["B, Steven", "Steven", "B", "admin"],
-						["Doe, Jon", "Jon", "Doe", "student"]
-						["B, Steven", "Steven", "B", "admin"],
-						["Doe, Jon", "Jon", "Doe", "student"]
-						["B, Steven", "Steven", "B", "admin"],
-						["Doe, Jon", "Jon", "Doe", "student"]
+						["Doe, Jon", "Jon", "Doe", "student"],
+						["de Boer, Jorrit", "Jorrit", "de Boer", "admin"],
+						["Doe, Jon", "Jon", "Doe", "student"],
+						["Musk, Elon", "Elon", "E", "admin"],
+						["Jobs, Steve", "Steve", "Jobs", "student"],
+						["Gates, Bill", "Bill", "Gates", "grade_admin"],
+						["Trump, Donald J", "Donald", "Trump", "student"]
 					];
 				case "evaluation":
 				case "enrollments":
@@ -136,13 +130,13 @@ function mapDispatchToProps(dispatch) {
 					return [
 						["displayName", "type", "course"],
 						["B, Steven", "decimal", "9"],
-						["T, Est", "decimal", "6"]
+						["T, Est", "decimal", "6"],
 						["B, Steven", "decimal", "9"],
-						["T, Est", "decimal", "6"]
+						["T, Est", "decimal", "6"],
 						["B, Steven", "decimal", "9"],
-						["T, Est", "decimal", "6"]
+						["T, Est", "decimal", "6"],
 						["B, Steven", "decimal", "9"],
-						["T, Est", "decimal", "6"]
+						["T, Est", "decimal", "6"],
 						["B, Steven", "decimal", "9"],
 						["T, Est", "decimal", "6"]
 					];
