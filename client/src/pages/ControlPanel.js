593965ebcd4ed43ef62108385d01a70278576ea3
jorritboer <jorritdb@hotmail.com>
2019-03-16 13:12:59 +0100

    Start controlPanel design

--- client/src/pages/ControlPanel.js
+++ client/src/pages/ControlPanel.js
@@ -9,6 +9,8 @@ import Typography from '@material-ui/core/Typography';
 import { connect } from 'react-redux';
 import { setAlias } from "../store/actions"
 import SelectUser from '../components/SelectUser';
+import { Divider } from '@material-ui/core';
+import Field from '../components/Field';
 
 class ControlPanel extends Component {
 
@@ -16,6 +18,10 @@ class ControlPanel extends Component {
 		super(props);
 		this.state = {
 			aliasId: null,
+			subject:{
+				name:"",
+				description:"",
+			},
 		}
 	}
 
@@ -29,22 +35,47 @@ class ControlPanel extends Component {
 		});
 	}
 
+
+	handleChange = (event) => {
+		this.setState({
+			subject: {
+				...this.state.group,
+				[event.name]: event.target.value,
+			}
+		});
+	}
+
 	render() {
 		return (
 			<Page>
-				<Paper
-					elevation={2}
-					style={{ position: "relative" }}
-				>
-					<Toolbar style={{ display: "flex" }}>
-						<Typography variant="subheading" color="textSecondary" style={{ flex: "2 1 auto" }}>
-							Beheer
-						</Typography>
-					</Toolbar>
-				</Paper>
 				<EnsureSecureLogin>
-					<SelectUser onChange={this.handleAliasChange} />
-					<Button variant="contained" color="primary" disabled={this.state.aliasId == null} onClick={this.loginUsingAlias}>Login in met alias</Button>
+					<Paper
+						elevation={2}
+						style={{ position: "relative" }}
+					>
+						<Toolbar style={{ display: "flex" }}>
+							<Typography variant="subheading" color="textSecondary" style={{ flex: "2 1 auto" }}>
+								Beheer
+							</Typography>
+						</Toolbar>
+					</Paper>
+					<div style={{margin:"10px 0"}}>
+						<Button variant="contained" color="primary" disabled={this.state.aliasId == null} onClick={this.loginUsingAlias}>Login in met alias</Button>
+					</div>
+					<Divider/>
+					<div style={{display:"flex" , flexDirection:"column"}}>
+						<div style={{display:"flex"}}>
+							<Typography variant="subheading" style={{flex: "1"}}>
+								Vak:
+							</Typography>
+							<Field label={"Naam"} editable={true} style={{flex:"5"}}/>
+						</div>
+							<Field  layout={{area:true}} value={this.state.subject.description} label={"Beschrijving"} name="description" onChange={this.handleChange} editable={true} style={{flex:"4"}}/>
+							<Button style={{width:"100px"}}>
+								add
+							</Button>
+					</div>
+					<Divider/>
 				</EnsureSecureLogin>
 			</Page>
 		);
--- client/src/store/mainReducer.js
+++ client/src/store/mainReducer.js
@@ -10,6 +10,7 @@ const DEFAULT_STATE = {
 	users: null,
 	showMenu: true,
 	notifications: [],
+	secureLogin: "a",
 	hasFetched: [],
 }
 
