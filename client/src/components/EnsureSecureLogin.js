18ce189146200ebcdf9400b1da65b21a5af6832d
StevenBrons <stevengoud@gmail.com>
2019-04-01 09:56:25 +0200

    Fix wrong ensure secure login message

--- client/src/components/EnsureSecureLogin.js
+++ client/src/components/EnsureSecureLogin.js
@@ -10,7 +10,7 @@ class EnsureSecureLogin extends Component {
 	render() {
 		if (!(this.props.active === false) && this.props.secureLogin == null) {
 			return <Paper style={{ padding: "20px", marginTop: "25px" }}>
-				<Field value="Log opnieuw in om de beoordelingen te bewerken" layout={{ area: true }} />
+				<Field value="Log opnieuw in om toegang te krijgen" layout={{ area: true }} />
 				<Button color="primary" variant="contained" onClick={() => {
 					setCookie("beforeLoginPath", window.location.pathname + window.location.search, 24);
 					if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
