a9b18f6f6655027227d2b3643df5c195b8950306
jorritboer <jorritdb@hotmail.com>
2019-04-04 17:13:23 +0200

    Add snackbar without redux state implementation

--- client/src/components/NotificationBar.js
+++ client/src/components/NotificationBar.js
@@ -1,96 +1,166 @@
 import React, { Component } from 'react';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import ErrorIcon from '@material-ui/icons/Error';
+import {IconButton, Snackbar, SnackbarContent} from '@material-ui/core/';
 import CloseIcon from '@material-ui/icons/Close';
-import Typography from '@material-ui/core/Typography';
 import { connect } from 'react-redux';
-import Paper from '@material-ui/core/Paper';
 
 import theme from '../lib/MuiTheme'
 import { removeNotification } from '../store/actions';
 
+		let testNotifications = [
+			{
+				priority: "medium",
+				type: "bar",
+				message: "A message",
+				scope:".",
+				//scope: "groep\/59\?tab=((Deelnemers)|(Inschrijvingen))"
+			},
+			{
+				priority: "high",
+				type:"bar",
+				message:"hoi",
+				scope: ".",
+			},
+			{
+				priority:"low",
+				type:"bar",
+				message:" doei",
+				scope: ".",
+			},
+			{
+				priority:"low",
+				type:"badge",
+				message: "dit zou je niet moeten zien",
+				scope:".",
+			},
+			{
+				priority:"high",
+				type:"bar",
+				message: "jajaa",
+			},
+			{
+				priority:"high",
+				type:"bar",
+				message: "jajadd",
+			},
+			{
+				priority:"high",
+				type:"bar",
+				message: "jajad",
+			},
+			{
+				priority:"high",
+				type:"bar",
+				message: "jaja94",
+			},
+		]
+
 class NotificationBar extends Component {
 
-	//dit hele component mag dus weg, misschien is het wel handig om dit als uitgangspunt te gebruiken
+	constructor(props) {
+		super(props);
+		let notifications = testNotifications.filter(this.checkNotification).reduce((nots,not,id) => {
+			not.open = true;
+			nots[id] = not;
+			return nots;
+		},{}) // map notifications to object with unique keys
+		this.state = {
+			notifications:notifications,
+			nextId:notifications.length,
+		}
+	}
 
+	checkNotification(not) {
+		try {
+			return not.type === "bar";// && new RegExp(not.scope).test(window.location.pathname);
+		} catch (err) {
+			return true;
+		}
+	}
+	closeNotification = (key) => {
+		this.setState( prevState => ({
+			...prevState,
+			notifications:{
+				...prevState.notifications,
+				[key]:{
+					...prevState.notifications[key],
+					open:false,
+				}
+			}
+		}))
+	}
+
+	removeNotification = (key) => {
+		let notifications = this.state.notifications;
+		delete notifications[key];
+		this.setState(prevState => ({
+			...prevState,
+			notifications: notifications,
+		}));
+	}
 
 	render() {
-		let className = "";
-		if (this.props.showMenu) {
-			className = "menuShown";
-		} else {
-			className = "menuHidden";
-		}
-
-		function checkNotification(not) {
-			try {
-				return not.type === "bar" && new RegExp(not.scope).test(window.location.pathname);
-			} catch (err) {
-				return true;
-			}
-		}
-
-		let notifications = this.props.notifications.filter(checkNotification).map((not) => {
-			let bg;
-			let fg;
+		let notifications = Object.keys(this.state.notifications).map((key,i) => {
+			const not = this.state.notifications[key];
+			let color;
 			switch (not.priority) {
-				case "low":
-					bg = theme.palette.background.paper;
-					fg = theme.palette.getContrastText(theme.palette.background.paper);
+				case "high":
+					color = theme.palette.error.main;
 					break;
 				case "medium":
-					bg = theme.palette.secondary.light;
-					fg = theme.palette.getContrastText(theme.palette.error.light);
+					color = "#ecef1f";
 					break;
-				case "high":
+				case "light":
 				default:
-					bg = theme.palette.error.dark;
-					fg = theme.palette.getContrastText(theme.palette.error.dark);
+					color = theme.palette.primary.main;
 					break;
 			}
 			return (
-				<Paper
-					elevation={16}
-					key={not.id}
-				>
-					<Toolbar style={{ backgroundColor: bg, height: "100%" }}>
-						<ErrorIcon style={{ color: fg, marginRight: "25px" }} />
-						<Typography variant="title" style={{ display: "inline-block", color: fg }}>
-							{not.message}
-						</Typography>
-						<Typography variant="title" style={{ display: "inline-block", color: fg }}>
-						</Typography>
-						<IconButton color="inherit" aria-label="Menu" onClick={() => this.props.removeNotification(not)} style={{ right: 25, position: "absolute" }}>
-							<CloseIcon style={{ color: fg }} />
-						</IconButton>
-					</Toolbar>
-				</Paper>
+				<Snackbar
+				key={key}
+				anchorOrigin={{
+				  vertical: "bottom",
+				  horizontal: "right"
+				}}
+				open={not.open}
+				//autoHideDuration={(i+1) * 3000}
+				onExited={() => {this.removeNotification(key)}}
+				style = {{marginBottom:i*65 + "px"}}
+				TransitionProps={{direction:"left"}}
+			  >
+			  <SnackbarContent
+			  	style={{background:color}}
+				message={<span id="message-id">{not.message}</span>}
+				action={[
+				  <IconButton key="close"
+					onClick={() => {this.closeNotification(key)}}
+				  >
+					<CloseIcon />
+				  </IconButton>
+				]}
+				/>
+			  </Snackbar>		
 			);
 		});
 		return (
-			<div className={"NotificationBar " + className}>
+			<div>
 				{notifications}
 			</div>
 		);
 	}
 }
 
-function mapStateToProps(state) {
-	return {
-		notifications: state.notifications,
-		showMenu: state.showMenu,
-	};
-}
+// function mapStateToProps(state) {
+// 	return {
+// 		notifications: state.notifications,
+// 	};
+// }
 
-function mapDispatchToProps(dispatch) {
-	return {
-		removeNotification: (notification) => dispatch(removeNotification(notification)),
-	};
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(NotificationBar);
+// function mapDispatchToProps(dispatch) {
+// 	return {
+// 		removeNotification: (notification) => dispatch(removeNotification(notification)),
+// 	};
+// }
 
 
-
+//export default connect(mapStateToProps, mapDispatchToProps)(NotificationBar);
+export default NotificationBar;
