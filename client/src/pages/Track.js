e2446b17bc29674890f001bc954cd090e92bbb5d
jorritboer <jorritdb@hotmail.com>
2019-08-14 17:16:36 +0200

    Add year picker and fix timeline css

--- client/src/pages/Track.js
+++ client/src/pages/Track.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import Page from "./Page";
-import { Typography, Toolbar, Paper, Button, withStyles } from '@material-ui/core';
+import { Typography, Toolbar, Paper, Button, withStyles, Tabs, Tab } from '@material-ui/core';
 import CheckIcon from "@material-ui/icons/CheckCircle";
 import ErrorIcon from "@material-ui/icons/Error";
 
 const Orange = "#f68620"; // should be taken from theme
 const Red = "#c4122f";
 
-const coursesPerPeriod = {
-	1: [1,2,3],
-	2: [4,5,6,7],
-	3: [8,9],
-	4: [10,11,12]
+const courseSchedule = {
+	4: {
+		1: [1,2,3],
+		2: [4,5,6,7],
+		3: [8,9],
+		4: [10,11,12]
+	},
+	5: {
+		4: [1,2,3],
+		3: [4,5,6,7],
+		2: [8,9],
+		1: [10,11,12]
+	},
+	6: {
+		3: [1,2,3],
+		2: [4,5,6,7],
+		4: [8,9],
+		1: [10,11,12]
+	},
 }
 
 const PTA = {
@@ -26,17 +40,17 @@ const PTA = {
 
 const courses = {
 	1 : "Object georienteerd programmeren",
-	2 : "Object georienteerd programmeren",
-	3 : "Object georienteerd programmeren",
-	4 : "Object georienteerd programmeren",
-	5 : "Object georienteerd programmeren",
-	6 : "Object georienteerd programmeren",
-	7 : "Object georienteerd programmeren",
-	8 : "Object georienteerd programmeren",
-	9 : "Object georienteerd programmeren",
-	10 : "Object georienteerd programmeren",
-	11 : "Object georienteerd programmeren",
-	12 : "Object georienteerd programmeren",
+	2 : "Filosofie",
+	3 : "Meetkunde",
+	4 : "Plato en Socrates",
+	5 : "Calculus",
+	6 : "Imperatief programmeren",
+	7 : "Website maken",
+	8 : "Engelse literatuur",
+	9 : "Nederlandse literatuur",
+	10 : "Grafieken",
+	11 : "Hardware",
+	12 : "HTML: De Basis",
 }
 
 class Track extends Component {
@@ -49,6 +63,7 @@ class Track extends Component {
 				3:null,
 				4:null,
 			},
+			year: 4,
 		}
 	}
 	trackAccepted = () => {
@@ -67,8 +82,16 @@ class Track extends Component {
 		})
 	}
 
+	changeYear = (e,value) => {
+		this.setState({
+			year:value,
+		})
+	}
+
 	render() {
+		const year = this.state.year;
 		const coursesSelected = this.state.coursesSelected;
+		const coursesPerPeriod = courseSchedule[year];
 		return (
 			<Page>
 				<Paper elevation={2} style={{ position: "relative" }}>
@@ -82,24 +105,39 @@ class Track extends Component {
 					</Toolbar>
 				</Paper>
 
-				<div style={{overflow:"scroll", display:"flex"}}>
-				{[4,5,6].map(y => { return(
+				<div style={{textAlign:"center"}}>
+					<Paper
+						style={{display:"inline-block", marginTop:"20px"}}
+						>
+						<Tabs
+							value={year}
+							indicatorColor="primary"
+							centered
+							onChange={this.changeYear}
+							>
+							<Tab value={4}  label="Klas 4"/>
+							<Tab value={5} label="Klas 5"/>
+							<Tab value={6} label="Klas 6"/>
+						</Tabs>
+					</Paper>
+				</div>
 				<div style={{marginTop:"15px"}}>
-					<div style={{height:"100px", display:"grid"}}>
-						<svg viewBox="0 0 950 100" style={{fill:Red, gridArea:"1/1", left:"0"}}>
-							<polygon points="0 0 950 0 950 20 0 100"/>
-						</svg>
-						<Typography variant="h4" style={{color:"white",gridArea:"1/1", margin:"5px"}}>Klas {y}</Typography>
+					<div style={{height:"30px", background:Red,
+				}}>
+						<Typography variant="h3" style={{color:"white", margin:"5px", padding:"4px"}}>Klas {year}</Typography>
 					</div>
-					<div style={{
-						display: "flex",
-					}}>
+					<div style={{ // triangle
+						height:"70px",
+						backgroundImage: "linear-gradient(to right bottom, " + Red + " 0%, " + Red + " 50%, transparent 51%)" }} // 51% to avoid pixelated edge
+						/>
+
+					<div style={{ display: "flex", }}>
 						{[1,2,3,4].map(p => {
 							return (
 							<div style = {{ borderLeft: "solid 6pt " + Red, flex:"1 1 auto" }}>
 								<div>
 									<Typography variant="h6"
-												style={{background:Red, color:"white", display:"inline-block"}}>
+												style={{background:Red, color:"white", display:"inline-block", paddingRight:"5px"}}>
 										Blok {p}
 									</Typography>
 								</div>
@@ -119,9 +157,6 @@ class Track extends Component {
 						})}
 					</div>
 				</div>
-				)})}
-				</div>
-
 			</Page>
 		)
 	}
