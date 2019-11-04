371e253acb69641857f8ebb12db6b5eb328c756d
jorritboer <jorritdb@hotmail.com>
2019-11-04 23:50:17 +0100

    Add styling information pop out

--- marketing_site/src/components/CourseInfo.js
+++ marketing_site/src/components/CourseInfo.js
@@ -4,24 +4,39 @@ import './CourseInfo.css';
 class CourseInfo extends Component {
 
   render() {
-	const course = this.props.course;
+		const course = this.props.course;
+		const id = 11;//TODO: replace with actual id
     return (
       <div className='CourseInfo'>
 				<div className='info-text'>
-					<h1>
-						Informatie over module {course.courseName} van {course.subjectName}
+					<h1 className='title'>
+						{course.courseName}
 					</h1>
-					<p>
-						Beschrijving: {course.courseDescription} <br/>
-						Leraar: {course.teacherName} <br/>
-						Studietijd: {course.studyTime} <br/>
-						Voorkennis: {course.remarks} <br/>
-						Doelgroep: {course.enrollableFor} <br/>
-						Periode: {course.period} <br/>
-						Dag: {course.day} <br/>
+
+					<p className='short-description'>
+						Hier een korte verleidelijke onderwerpregel
 					</p>
+
+					<div className='period'>
+						<div className='square'/>
+						<p className='number'>
+							Blok {course.period}
+						</p>
+					</div>
+
+					<p className='description-title'>
+						Module: Beschrijving
+					</p>
+
+					<p>
+						{course.courseDescription}
+					</p>
+
+					<button className='enroll-button'>
+						AANMELDEN
+					</button>
 				</div>
-				<div className='image'/>
+				<div className='image' style={{background:`url(https://q-highschool.nl/wp-content/uploads/thumbnail_course_${id}.jpg)`}}/>
 				<button 
 					className='close-button' 
 					onClick={this.props.onClose} 
