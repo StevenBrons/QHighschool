60975aead67e933e3ae1010c5b67728427c81680
jorritboer <jorritdb@hotmail.com>
2019-11-10 12:49:04 +0100

    Add scrolling functionality

--- marketing_site/src/components/CourseGroup.js
+++ marketing_site/src/components/CourseGroup.js
@@ -4,17 +4,41 @@ import './CourseGroup.css';
 
 class CourseGroup extends Component {
 
+	constructor(props) {
+		super(props);
+		this.scroller = React.createRef();
+	}
+
+	scrollToLeft = () => {
+		this.scroller.current.scrollLeft -= this.scroller.current.offsetWidth * 0.96; // * 0.96 because there is 2% on both sides
+	}
+
+	scrollToRight = () => {
+		this.scroller.current.scrollLeft += this.scroller.current.offsetWidth  * 0.96; // * 0.96 because there is 2% on both sides
+	}
+
 	render() {
 		const {courses, selectedCourse} = this.props;
 		return (
-			<div className='CourseGroup'>
+			<div 
+				className='CourseGroup'
+			>
 				<h3
 					className='title'
 					onClick={()=>{}}//TODO
 				>
 					{this.props.title}
 				</h3>
-				<div>
+				<button
+					className='scroll-left'
+					onClick={this.scrollToLeft}
+				>
+					{'<'}
+				</button>
+				<div
+					ref={this.scroller}
+					className='scroller'
+				>
 					{Object.keys(courses).map(id => {
 						return <Course 
 									key={id} 
@@ -26,6 +50,12 @@ class CourseGroup extends Component {
 								/>
 					})}
 				</div>
+				<button
+					className='scroll-right'
+					onClick={this.scrollToRight}
+				>
+					{'>'}
+				</button>
 			</div>
 		)
 	}
