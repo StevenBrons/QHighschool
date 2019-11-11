729566d0afc977fa4182c0890aa364f66d2cd6a2
jorritboer <jorritdb@hotmail.com>
2019-11-11 17:26:22 +0100

    Make scrolling consistent

--- marketing_site/src/components/CourseGroup.js
+++ marketing_site/src/components/CourseGroup.js
@@ -7,37 +7,49 @@ class CourseGroup extends Component {
 	constructor(props) {
 		super(props);
 		this.scroller = React.createRef();
+		this.title = React.createRef();
+		let maxPage = Math.floor(Object.keys(props.courses).length / 4);
+		this.state = {
+			page: 0,
+			maxPage: maxPage
+		}
+		if (maxPage > 0) window.addEventListener('resize', () => this.scrollToPage(this.state.page));
 	}
 
-	scrollToLeft = () => {
-		this.scroller.current.scrollLeft -= this.scroller.current.offsetWidth * 0.96; // * 0.96 because there is 2% on both sides
-	}
-
-	scrollToRight = () => {
-		this.scroller.current.scrollLeft += this.scroller.current.offsetWidth  * 0.96; // * 0.96 because there is 2% on both sides
+	scrollToPage(page) {
+		let scroller = this.scroller.current;
+		const margin = this.title.current.offsetLeft;
+		scroller.scrollLeft = scroller.children[4*page].offsetLeft - margin;
+		this.setState({
+			page: page
+		})
 	}
 
 	render() {
 		const {courses, selectedCourse} = this.props;
+		const {page, maxPage} = this.state;
 		return (
 			<div 
 				className='CourseGroup'
 			>
 				<h3
 					className='title'
+					ref={this.title}
 					onClick={()=>{}}//TODO
 				>
 					{this.props.title}
 				</h3>
-				<button
-					className='scroll-button left'
-					onClick={this.scrollToLeft}
-				>
-					{'\u276e'}
-				</button>
+				{ page !== 0 && 
+					<button
+						className='scroll-button left'
+						onClick={() => this.scrollToPage(page-1)}
+					>
+						{'\u276e'}
+					</button>
+				}
 				<div
-					ref={this.scroller}
 					className='scroller'
+					ref={this.scroller}
 				>
 					{Object.keys(courses).map(id => {
 						return <Course 
@@ -50,12 +62,14 @@ class CourseGroup extends Component {
 								/>
 					})}
 				</div>
-				<button
-					className='scroll-button right'
-					onClick={this.scrollToRight}
-				>
-					{'\u276f'}
-				</button>
+				{page !== maxPage && 
+					<button
+						className='scroll-button right'
+						onClick={() => this.scrollToPage(page+1)}
+					>
+						{'\u276f'}
+					</button>
+				}
 			</div>
 		)
 	}
