b3bb8397eec526af5a4243410cbda557851e7ac1
jorritboer <jorritdb@hotmail.com>
2019-11-12 17:05:37 +0100

    Finish subject info design

--- marketing_site/src/components/SubjectInfo.js
+++ marketing_site/src/components/SubjectInfo.js
@@ -9,23 +9,36 @@ class CourseInfo extends Component {
 			<div
 				className='SubjectInfo'
 			>
-				{previousSubject && 
-				<button
-					className='previous-subject'
-					onClick={previousSubject}
-				>
-					{'<'}
-				</button>
-				}
-				<h1>HOI HIER IS INFORMATIE OVER {subject}</h1>
-				{nextSubject && 
-				<button
-					className='next-subject'
-					onClick={nextSubject}
-				>
-					{'>'}
-				</button>
-				}
+				<div className='button-container'>
+					{previousSubject && 
+					<div
+						className='button previous-subject'
+						onClick={previousSubject}
+					/>
+					}
+				</div>
+				<div className='text'>
+					<h1 className='title'>Waar gaat het vak {subject.toLowerCase()} over?</h1>
+					<p className='tempting-sentence'>
+						Hier een korte verleidelijke onderwerpregel
+					</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+						Suspendisse enim quam, varius sit amet mollis et, aliquam at dui.
+						 In hac habitasse platea dictumst. Vestibulum rutrum convallis elit, vitae placerat nunc rutrum sit amet.
+						  In ut turpis suscipit, convallis dui convallis, fermentum enim.
+							 Nam elit risus, aliquet posuere neque ut, faucibus vehicula purus.
+							  Cras mollis erat orci, in sodales augue varius eget.
+							  Pellentesque faucibus diam eu arcu congue finibus. Pellentesque feugiat eget mauris sed placerat.</p>
+
+				</div>
+				<div className='button-container'>
+					{nextSubject && 
+					<div
+						className='button next-subject'
+						onClick={nextSubject}
+					/>
+					}
+				</div>
 				<button 
 					className='close-button' 
 					onClick={onClose} 
