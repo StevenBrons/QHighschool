44d176c8298356ff52d2169d54fec6bb4492b3f2
StevenBrons <smp15.programming@gmail.com>
2019-11-22 12:57:50 +0100

    Fix issues from refactor

--- api/app.js
+++ api/app.js
@@ -1,51 +1,53 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cookieSession = require('cookie-session');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cookieSession = require("cookie-session");
 const passport = require("passport");
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json.js');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-const apiRoute = require('./routes/apiRoute');
-const authRoute = require('./routes/authRoute');
-const keys = require('./private/keys');
-const bodyParser = require('body-parser');
-var flash = require('connect-flash');
+const apiRoute = require("./routes/apiRoute");
+const authRoute = require("./routes/authRoute");
+const keys = require("./private/keys");
+const bodyParser = require("body-parser");
+var flash = require("connect-flash");
 
-require('./dec/MainDec');
-require('./dec/UserDec');
-require('./dec/CourseDec');
-require('./dec/SubjectDec');
-require('./dec/CourseGroupDec');
-require('./dec/EnrollmentDec');
-require('./dec/EvaluationDec');
-require('./dec/LessonDec');
-require('./dec/PresenceDec');
-require('./dec/NotificationDec');
-require('./dec/LoggedInDec');
+require("./dec/MainDec");
+require("./dec/UserDec");
+require("./dec/CourseDec");
+require("./dec/SubjectDec");
+require("./dec/CourseGroupDec");
+require("./dec/EnrollmentDec");
+require("./dec/EvaluationDec");
+require("./dec/LessonDec");
+require("./dec/PresenceDec");
+require("./dec/NotificationDec");
+require("./dec/LoggedInDec");
 
-require('./lib/passportSetup');
-require('./lib/taxi');
-require('./office/graphConnection');
+require("./lib/passportSetup");
+require("./lib/taxi");
+require("./office/graphConnection");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cookieSession({
-	keys: [keys.sessionSecret],
-	maxAge: 7 * 24 * 60 * 60 * 1000,
-}));
+app.use(
+  cookieSession({
+    keys: [keys.sessionSecret],
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  })
+);
 
 app.use(flash());
 
@@ -55,21 +57,24 @@ app.use(passport.session());
 app.use("/api", apiRoute);
 app.use("/auth", authRoute);
 app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/public", express.static(path.join(__dirname, "public")))
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/profiel", (req, res) => {
-	res.redirect('http://localhost:3000/profiel/?from=login&secureLogin=' + req.query.secureLogin);
+  res.redirect(
+    "http://localhost:3000/profiel/?from=login&secureLogin=" +
+      req.query.secureLogin
+  );
 });
 
 app.use((req, res, next) => {
-	next(createError(404));
+  next(createError(404));
 });
 
 app.use((err, req, res, next) => {
-	res.status(err.status || 500);
-	res.send({
-		success: false,
-		error: err.message,
-	});
+  res.status(err.status || 500);
+  res.send({
+    success: false,
+    error: err.message
+  });
 });
 
 module.exports = app;
--- api/routes/functionRoute.js
+++ api/routes/functionRoute.js
@@ -1,50 +1,76 @@
 var express = require("express");
 var router = express.Router();
-var functionDb = require('../database/FunctionDB');
-var taxi = require('../lib/taxi');
-const { promiseMiddleware, doSuccess, doReturn } = require('./handlers');
-const { ensureAdmin, ensureConfirm, ensureSecure, ensureGradeAdmin } = require('./permissions');
+var functionDb = require("../database/FunctionDB");
+var taxi = require("../lib/taxi");
+const { promiseMiddleware, doSuccess, doReturn } = require("./handlers");
+const {
+  ensureAdmin,
+  ensureConfirm,
+  ensureSecure,
+  ensureGradeAdmin
+} = require("./permissions");
 
-router.post("/updateAllGroups", ensureAdmin, ensureConfirm, promiseMiddleware(() => {
-	console.log("Updating all groups!");
-	return functionDb.updateAllGroups();
-}), doSuccess);
+router.post(
+  "/updateAllGroups",
+  ensureAdmin,
+  ensureConfirm,
+  promiseMiddleware(() => {
+    console.log("Updating all groups!");
+    return functionDb.updateAllGroups();
+  }),
+  doSuccess
+);
 
-router.post("/taxi", promiseMiddleware((req) => {
-	if (req.user.isAdmin()) {
-		return taxi.getSchedule(-1, parseInt(req.body.week));
-	} else {
-		return taxi.getSchedule(req.user.id, parseInt(req.body.week));
-	}
-}), doReturn);
+router.post(
+  "/taxi",
+  promiseMiddleware(req => {
+    if (req.user.isAdmin()) {
+      return taxi.getSchedule(-1, parseInt(req.body.week));
+    } else {
+      return taxi.getSchedule(req.user.id, parseInt(req.body.week));
+    }
+  }),
+  doReturn
+);
 
-router.post("/alias", ensureAdmin, ensureSecure, promiseMiddleware((req, res) => {
-	if (Number.isInteger(req.user.id) && req.user.id >= 0) {
-		return functionDb.setAlias(req.user.token, req.user.id, req.body.userId);
-	} else {
-		throw new Error("Invalid data types");
-	}
-}), doSuccess);
-
+router.post(
+  "/alias",
+  ensureAdmin,
+  ensureSecure,
+  promiseMiddleware((req, res) => {
+    if (Number.isInteger(req.user.id) && req.user.id >= 0) {
+      return functionDb.setAlias(req.user.token, req.user.id, req.body.userId);
+    } else {
+      throw new Error("Invalid data types");
+    }
+  }),
+  doSuccess
+);
 
 async function formatInTable(array) {
-	const keys = Object.keys(array[0]);
-	return [keys, ...array.map(obj => keys.map(key => obj[key]))]
+  const keys = Object.keys(array[0]);
+  return [keys, ...array.map(obj => keys.map(key => obj[key]))];
 }
 
-router.post("/data", ensureSecure, ensureGradeAdmin, promiseMiddleware((req, res) => {
-	const table = req.body.table; //evaluation,users,enrollment
-	const school = req.user.school;
-	switch (table) {
-		case "evaluations":
-			return functionDb.getEvaluation(school).then(formatInTable);
-		case "enrollments":
-			return functionDb.getEnrollment(school).then(formatInTable);
-		case "users":
-			return functionDb.getUserData(school).then(formatInTable);
-		default:
-			throw new Error("invalid table: " + table);
-	}
-}), doReturn);
+router.post(
+  "/data",
+  ensureSecure,
+  ensureGradeAdmin,
+  promiseMiddleware((req, res) => {
+    const table = req.body.table; //evaluation,users,enrollment
+    const school = req.user.school;
+    switch (table) {
+      case "evaluations":
+        return functionDb.getEvaluation(school).then(formatInTable);
+      case "enrollments":
+        return functionDb.getEnrollment(school).then(formatInTable);
+      case "users":
+        return functionDb.getUserData(school).then(formatInTable);
+      default:
+        throw new Error("invalid table: " + table);
+    }
+  }),
+  doReturn
+);
 
 module.exports = router;
