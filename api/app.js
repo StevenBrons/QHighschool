b2b3cfb77d4f9f00a550535f40a002a9659f2393
StevenBrons <smp15.programming@gmail.com>
2019-11-22 13:40:52 +0100

    WIP

--- api/app.js
+++ api/app.js
@@ -41,6 +41,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(
   cookieSession({
