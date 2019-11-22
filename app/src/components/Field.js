77c3970428cc85fdeda2f7776868716a5ad74538
StevenBrons <smp15.programming@gmail.com>
2019-11-22 16:26:48 +0100

    Fix SelectUser component popper

--- app/src/components/Field.js
+++ app/src/components/Field.js
@@ -1,299 +1,312 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import theme from '../lib/MuiTheme'
-import MenuItem from '@material-ui/core/MenuItem';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import PropTypes from 'prop-types';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import theme from "../lib/MuiTheme";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import PropTypes from "prop-types";
 
 class Field extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false
+    };
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			error: false,
-		}
-	}
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return {
+      ...prevState,
+      error: nextProps.editable
+        ? !Field.validate(nextProps.value, nextProps.validate)
+        : false
+    };
+  }
 
-	static getDerivedStateFromProps(nextProps, prevState) {
-		return {
-			...prevState,
-			error: nextProps.editable ? !Field.validate(nextProps.value, nextProps.validate) : false,
-		}
-	}
+  static validate(value, rules = {}, options = [value]) {
+    switch (rules.type) {
+      case "phoneNumber":
+        const re = /(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)/i;
+        if (!re.test(value)) {
+          return false;
+        }
+        break;
+      case "email":
+        const re2 = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g;
+        if (!re2.test(value)) {
+          return false;
+        }
+        break;
+      case "integer":
+        if (!/^\+?[1-9][\d]*$/i.test(value)) {
+          return false;
+        }
+        break;
+      case "float":
+        if (isNaN(value)) {
+          return false;
+        }
+        break;
+      case "decimalGrade":
+        //replace dot with comma and vice versa
+        const x = value
+          .replace(/\./g, "_$comma$_")
+          .replace(/,/g, ".")
+          .replace(/_\$comma\$_/g, ",");
+        if (x === "ND") {
+          return true;
+        }
+        if (isNaN(x)) {
+          return false;
+        }
+        break;
+      default:
+        break;
+    }
+    if (rules.min) {
+      if (parseFloat(value) < rules.min) {
+        return false;
+      }
+    }
+    if (rules.max) {
+      if (parseFloat(value) > rules.max) {
+        return false;
+      }
+    }
+    if (rules.notEmpty) {
+      if (
+        value == null ||
+        value === "" ||
+        value === " " ||
+        options.indexOf(value) === -1
+      ) {
+        return false;
+      }
+    }
+    if (rules.maxLength && value != null) {
+      if (value.length > rules.maxLength) {
+        return false;
+      }
+    }
 
-	static validate(value, rules = {}, options = [value]) {
-		switch (rules.type) {
-			case "phoneNumber":
-				const re = /(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)/i;
-				if (!re.test(value)) {
-					return false;
-				}
-				break;
-			case "email":
-				const re2 = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g;
-				if (!re2.test(value)) {
-					return false;
-				}
-				break;
-			case "integer":
-				if (!/^\+?[1-9][\d]*$/i.test(value)) {
-					return false;
-				}
-				break;
-			case "float":
-				if (isNaN(value)) {
-					return false;
-				}
-				break;
-			case "decimalGrade":
-				//replace dot with comma and vice versa
-				const x = value.replace(/\./g, "_$comma$_").replace(/,/g, ".").replace(/_\$comma\$_/g, ",");
-				if (x === "ND") {
-					return true;
-				}
-				if (isNaN(x)) {
-					return false;
-				}
-				break;
-			default: break;
-		}
-		if (rules.min) {
-			if (parseFloat(value) < rules.min) {
-				return false;
-			}
-		}
-		if (rules.max) {
-			if (parseFloat(value) > rules.max) {
-				return false;
-			}
-		}
-		if (rules.notEmpty) {
-			if (value == null || value === "" || value === " " || options.indexOf(value) === -1) {
-				return false;
-			}
-		}
-		if (rules.maxLength && value != null) {
-			if (value.length > rules.maxLength) {
-				return false;
-			}
-		}
+    return true;
+  }
 
-		return true;
-	}
+  onChange = event => {
+    this.props.onChange(event.target.value);
+  };
 
-	onChange = (event) => {
-		this.props.onChange(event.target.value);
-	}
+  render() {
+    let value = this.props.value == null ? "" : this.props.value;
+    let style = this.props.style || {};
+    let layout = this.props.layout || {};
 
-	render() {
-		let value = this.props.value == null ? "" : this.props.value;
-		let style = this.props.style || {};
-		let layout = this.props.layout || {};
+    let label = this.props.label;
+    let disabled = true;
+    let multiline = false;
+    let fullWidth = false;
 
-		let label = this.props.label;
-		let disabled = true;
-		let multiline = false;
-		let fullWidth = false;
+    let options = this.props.options;
+    let disableUnderline = style.underline || true;
+    let margin = style.margin || "none";
+    let marginPx = 0;
+    let menuItems;
+    let endAdornment;
+    let classNames = [];
+    if (this.props.style) {
+      switch (this.props.style.type) {
+        case "headline":
+          style = {
+            ...style,
+            ...theme.typography.headline
+          };
+          style.color = theme.palette.primary.main;
+          break;
+        case "title":
+          style = {
+            ...style,
+            ...theme.typography.title
+          };
+          style.color = theme.palette.primary.main;
+          break;
+        case "caption":
+          style.color = theme.palette.text.secondary;
+          break;
+        default:
+          style.color = theme.palette.text.primary;
+          break;
+      }
+      switch (this.props.style.color) {
+        case "primary":
+          style.color = theme.palette.primary.main;
+          break;
+        case "primaryContrast":
+          style.color = theme.palette.primary.contrastText;
+          break;
+        case "secondary":
+          style.color = theme.palette.secondary.main;
+          break;
+        case "secondaryContrast":
+          style.color = theme.palette.secondary.contrastText;
+          break;
+        case "error":
+          style.color = theme.palette.error.main;
+          break;
+        default:
+          break;
+      }
+    }
 
-		let options = this.props.options;
-		let disableUnderline = style.underline || true;
-		let margin = style.margin || "none";
-		let marginPx = 0;
-		let menuItems;
-		let endAdornment;
-		let classNames = [];
-		if (this.props.style) {
-			switch (this.props.style.type) {
-				case "headline":
-					style = {
-						...style,
-						...theme.typography.headline,
-					};
-					style.color = theme.palette.primary.main;
-					break;
-				case "title":
-					style = {
-						...style,
-						...theme.typography.title
-					};
-					style.color = theme.palette.primary.main;
-					break;
-				case "caption":
-					style.color = theme.palette.text.secondary;
-					break;
-				default:
-					style.color = theme.palette.text.primary;
-					break;
-			}
-			switch (this.props.style.color) {
-				case "primary":
-					style.color = theme.palette.primary.main;
-					break;
-				case "primaryContrast":
-					style.color = theme.palette.primary.contrastText;
-					break;
-				case "secondary":
-					style.color = theme.palette.secondary.main;
-					break;
-				case "secondaryContrast":
-					style.color = theme.palette.secondary.contrastText;
-					break;
-				case "error":
-					style.color = theme.palette.error.main;
-					break;
-				default:
-					break;
-			}
-		}
+    if (layout.alignment === "right") {
+      style.float = "right";
+      style.textAlign = "right";
+      classNames.push("right");
+    }
+    if (layout.alignment === "left") {
+      style.float = "left";
+    }
 
-		if (layout.alignment === "right") {
-			style.float = "right";
-			style.textAlign = "right";
-			classNames.push("right");
-		}
-		if (layout.alignment === "left") {
-			style.float = "left";
-		}
+    if (layout.area) {
+      fullWidth = true;
+      multiline = true;
+    }
 
-		if (layout.area) {
-			fullWidth = true;
-			multiline = true;
-		}
+    if (
+      this.props.default &&
+      this.props.editable === false &&
+      (value === "" || value == null)
+    ) {
+      value = this.props.default;
+    }
 
-		if (this.props.default && this.props.editable === false && (value === "" || value == null)) {
-			value = this.props.default;
-		}
+    if (this.props.editable) {
+      disabled = false;
+      disableUnderline = style.underline || false;
+      margin = style.margin || "normal";
+    } else {
+      if (!(style.labelVisible === true)) {
+        label = null;
+      }
+      if (
+        options != null &&
+        options[0] != null &&
+        typeof options[0] !== "string"
+      ) {
+        const a = options.filter(opt => {
+          return opt.value === value;
+        })[0];
+        if (a != null) {
+          value = a.label;
+        }
+      }
+      options = null;
+    }
 
-		if (this.props.editable) {
-			disabled = false;
-			disableUnderline = style.underline || false;
-			margin = style.margin || "normal";
-		} else {
-			if (!(style.labelVisible === true)) {
-				label = null;
-			}
-			if (options != null && options[0] != null && typeof options[0] !== "string") {
-				const a = options.filter((opt) => {
-					return opt.value === value;
-				})[0];
-				if (a != null) {
-					value = a.label;
-				}
-			}
-			options = null;
-		}
+    if (options) {
+      menuItems = options.map(option => {
+        if (typeof option !== "string") {
+          return (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          );
+        }
+        return (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        );
+      });
+    }
+    if (style.unit) {
+      endAdornment = (
+        <InputAdornment position="end">{style.unit}</InputAdornment>
+      );
+    }
+    switch (margin) {
+      case "dense":
+        marginPx = 5;
+        break;
+      case "normal":
+        marginPx = 12;
+        break;
+      default:
+    }
 
-		if (options) {
-			menuItems = options.map(option => {
-				if (typeof option !== "string") {
-					return (
-						<MenuItem key={option.value} value={option.value}>
-							{option.label}
-						</MenuItem>
-					)
-				}
-				return (
-					<MenuItem key={option} value={option}>
-						{option}
-					</MenuItem>
-				)
-			});
-		}
-		if (style.unit) {
-			endAdornment = <InputAdornment position="end">{style.unit}</InputAdornment>;
-		}
-		switch (margin) {
-			case "dense":
-				marginPx = 5;
-				break;
-			case "normal":
-				marginPx = 12;
-				break;
-			default:
-		}
-
-		const field = (
-			<TextField
-				value={value}
-				margin="none"
-				disabled={disabled}
-				fullWidth={fullWidth}
-				multiline={multiline}
-				className={classNames.join(" ") + " Field"}
-				label={label}
-				select={options ? true : false}
-				style={{ flex: 1, ...style, margin: marginPx }}
-				onChange={this.onChange}
-				error={this.state.error}
-				InputProps={{
-					disableUnderline,
-					style,
-					endAdornment: endAdornment ? endAdornment : null,
-					inputProps: {
-						style,
-					}
-				}}
-
-			>
-				{menuItems}
-			</TextField>
-		);
-		if (layout.td) {
-			// style.width = undefined;
-			return (
-				<td style={style}>
-					{field}
-				</td>
-			);
-		} else {
-			return field;
-		}
-	}
-
+    const field = (
+      <TextField
+        id={this.props.id}
+        value={value}
+        margin="none"
+        disabled={disabled}
+        fullWidth={fullWidth}
+        multiline={multiline}
+        className={classNames.join(" ") + " Field"}
+        label={label}
+        select={options ? true : false}
+        style={{ flex: 1, ...style, margin: marginPx }}
+        onChange={this.onChange}
+        error={this.state.error}
+        InputProps={{
+          disableUnderline,
+          style,
+          endAdornment: endAdornment ? endAdornment : null,
+          inputProps: {
+            style
+          }
+        }}
+      >
+        {menuItems}
+      </TextField>
+    );
+    if (layout.td) {
+      // style.width = undefined;
+      return <td style={style}>{field}</td>;
+    } else {
+      return field;
+    }
+  }
 }
 
-
 Field.propTypes = {
-	validate: PropTypes.shape({
-		min: PropTypes.number,
-		max: PropTypes.number,
-		type: PropTypes.oneOf(["phoneNumber", "email", "integer", "decimalGrade"]),
-		notEmpty: PropTypes.bool,
-		maxLength: PropTypes.number,
-	}),
-	value: PropTypes.oneOfType([
-		PropTypes.string,
-		PropTypes.number,
-	]),
-	style: PropTypes.shape({
-		labelVisible: PropTypes.bool,
-		type: PropTypes.oneOf([
-			"title",
-			"caption",
-			"headline",
-		]),
-		underline: PropTypes.bool,
-		color: PropTypes.oneOf(["primary", "primaryContrast", "secondary", "error"]),
-		unit: PropTypes.string,
-		margin: PropTypes.oneOf(["none", "dense", "normal"]),
-	}),
-	layout: PropTypes.shape({
-		area: PropTypes.bool,
-		alignment: PropTypes.oneOf(["left", "right"]),
-		td: PropTypes.bool,
-	}),
-	label: PropTypes.string,
-	options: PropTypes.any,
-	// PropTypes.arrayOf(
-	// 	PropTypes.oneOfType([
-	// 		PropTypes.shape({
-	// 			value: PropTypes.string,
-	// 			label: PropTypes.string,
-	// 		}),
-	// 		PropTypes.string,
-	// 	])),
-	editable: PropTypes.bool,
-	default: PropTypes.string,
+  validate: PropTypes.shape({
+    min: PropTypes.number,
+    max: PropTypes.number,
+    type: PropTypes.oneOf(["phoneNumber", "email", "integer", "decimalGrade"]),
+    notEmpty: PropTypes.bool,
+    maxLength: PropTypes.number
+  }),
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  style: PropTypes.shape({
+    labelVisible: PropTypes.bool,
+    type: PropTypes.oneOf(["title", "caption", "headline"]),
+    underline: PropTypes.bool,
+    color: PropTypes.oneOf([
+      "primary",
+      "primaryContrast",
+      "secondary",
+      "error"
+    ]),
+    unit: PropTypes.string,
+    margin: PropTypes.oneOf(["none", "dense", "normal"])
+  }),
+  layout: PropTypes.shape({
+    area: PropTypes.bool,
+    alignment: PropTypes.oneOf(["left", "right"]),
+    td: PropTypes.bool
+  }),
+  id: PropTypes.string,
+  label: PropTypes.string,
+  options: PropTypes.any,
+  // PropTypes.arrayOf(
+  // 	PropTypes.oneOfType([
+  // 		PropTypes.shape({
+  // 			value: PropTypes.string,
+  // 			label: PropTypes.string,
+  // 		}),
+  // 		PropTypes.string,
+  // 	])),
+  editable: PropTypes.bool,
+  default: PropTypes.string
 };
 
 export default Field;
--- app/src/components/SelectUser.js
+++ app/src/components/SelectUser.js
@@ -1,96 +1,108 @@
-import React, { Component } from 'react';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import Popper from '@material-ui/core/Popper';
-import Paper from '@material-ui/core/Paper';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import Popper from "@material-ui/core/Popper";
+import Paper from "@material-ui/core/Paper";
+import { connect } from "react-redux";
 import filter from "lodash/filter";
-import { ClickAwayListener } from '@material-ui/core';
+import { ClickAwayListener } from "@material-ui/core";
 
-import Field from './Field';
-import { getAllUsers } from '../store/actions';
+import Field from "./Field";
+import { getAllUsers } from "../store/actions";
 
 class SelectUser extends Component {
+  constructor(props) {
+    super(props);
+    this.myref = this.state = {
+      input: "",
+      userId: null,
+      open: false,
+      id: "SelectUser" + Math.floor(Math.random() * 1000)
+    };
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			input: "",
-			userId: null,
-			open: false,
-		}
-	}
+  componentDidMount() {
+    this.props.getAllUsers();
+  }
 
-	componentDidMount() {
-		this.props.getAllUsers();
-	}
+  selectUser = (userId, displayName) => {
+    this.setState({
+      input: displayName,
+      open: false,
+      userId: userId
+    });
+    this.props.onChange(userId, displayName);
+  };
 
-	selectUser = (userId, displayName) => {
-		this.setState({
-			input: displayName,
-			open: false,
-			userId: userId,
-		});
-		this.props.onChange(userId, displayName);
-	}
+  getFittingUsers = () => {
+    const regExp = new RegExp(this.state.input.toLowerCase());
+    let count = 0;
+    return filter(this.props.users, user => {
+      const v = regExp.test(user.displayName.toLowerCase());
+      if (v) {
+        count++;
+      }
+      return v && count < 6;
+    }).map(user => {
+      return (
+        <MenuItem
+          key={user.id + user.displayName}
+          onClick={() => this.selectUser(user.id, user.displayName)}
+        >
+          {user.displayName}
+        </MenuItem>
+      );
+    });
+  };
 
-	getFittingUsers = () => {
-		const regExp = new RegExp(this.state.input.toLowerCase());
-		let count = 0;
-		return filter(this.props.users, (user) => {
-			const v = regExp.test(user.displayName.toLowerCase())
-			if (v) {
-				count++;
-			}
-			return v && count < 6;
-		}).map(user => {
-			return (
-				<MenuItem key={user.id + user.displayName} onClick={() => this.selectUser(user.id, user.displayName)}>
-					{user.displayName}
-				</MenuItem>
-			);
-		});
-	}
+  handleInput = value => {
+    if (this.state.userId) {
+      this.props.onChange(null, null);
+    }
+    this.setState({
+      input: value,
+      open: true,
+      userId: null
+    });
+  };
 
-	handleInput = (value) => {
-		if (this.state.userId) {
-			this.props.onChange(null, null);
-		}
-		this.setState({
-			input: value,
-			open: true,
-			userId: null,
-		});
-	}
-
-	render() {
-		return (
-			<ClickAwayListener onClickAway={() => this.setState({ open: false })}>
-				<span>
-					<Field label="Gebruiker" value={this.state.input} editable onChange={this.handleInput} style={{ margin: "normal" }} />
-					<Popper open={this.state.aliasId == null && this.state.open} disablePortal style={{ position: "fixed", zIndex: "1" }} >
-						<Paper>
-							<MenuList>
-								{this.getFittingUsers()}
-							</MenuList>
-						</Paper>
-					</Popper>
-				</span>
-			</ClickAwayListener>
-		);
-	}
+  render() {
+    return (
+      <ClickAwayListener onClickAway={() => this.setState({ open: false })}>
+        <span>
+          <Field
+            label="Gebruiker"
+            value={this.state.input}
+            editable
+            onChange={this.handleInput}
+            style={{ margin: "normal" }}
+            id={this.state.id}
+          />
+          <Popper
+            open={this.state.aliasId == null && this.state.open}
+            disablePortal
+            anchorEl={document.getElementById(this.state.id)}
+            style={{ position: "fixed", zIndex: "1" }}
+          >
+            <Paper>
+              <MenuList>{this.getFittingUsers()}</MenuList>
+            </Paper>
+          </Popper>
+        </span>
+      </ClickAwayListener>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		users: state.users,
-	}
+  return {
+    users: state.users
+  };
 }
 function mapDispatchToProps(dispatch) {
-	return {
-		getAllUsers: () => dispatch(getAllUsers()),
-	};
+  return {
+    getAllUsers: () => dispatch(getAllUsers())
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SelectUser);
-
