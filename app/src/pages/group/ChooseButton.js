30b9edf834bdb5c07e5f4991603b80a8fa00020b
StevenBrons <smp15.programming@gmail.com>
2019-11-24 15:47:28 +0100

    Update enrollment popup

--- app/src/pages/group/ChooseButton.js
+++ app/src/pages/group/ChooseButton.js
@@ -1,121 +1,139 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import Clear from '@material-ui/icons/Clear';
-import { connect } from 'react-redux';
-import { toggleEnrollment, getEnrollableGroups, getEnrolLments } from '../../store/actions';
-import Progress from '../../components/Progress';
-import EnrollmentPopup from './EnrollmentPopup';
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import Clear from "@material-ui/icons/Clear";
+import { connect } from "react-redux";
+import {
+  toggleEnrollment,
+  getEnrollableGroups,
+  getEnrolLments
+} from "../../store/actions";
+import Progress from "../../components/Progress";
+import EnrollmentPopup from "./EnrollmentPopup";
 
 class ChooseButton extends Component {
+  constructor(props) {
+    super(props);
+    this.props.getEnrollableGroups();
+    this.props.getEnrolLments();
+    this.state = {
+      dialogOpen: false
+    };
+  }
 
-	constructor(props) {
-		super(props);
-		this.props.getEnrollableGroups();
-		this.props.getEnrolLments();
-		this.state = {
-			dialogOpen: false,
-		}
-	}
+  preventPopupClose = preventClose => {
+    this.preventClose = preventClose;
+  };
 
-	preventPopupClose(preventClose) {
-		this.preventClose = preventClose;
-	}
+  handlePopup = doEnroll => {
+    if (doEnroll) {
+      this.props.toggleEnrollment(this.props.group);
+    }
+    if (!this.preventClose) {
+      this.setState({
+        dialogOpen: !this.state.dialogOpen
+      });
+    }
+  };
 
-	handlePopup(doEnroll) {
-		if (doEnroll) {
-			this.props.toggleEnrollment(this.props.group);
-		}
-		if (!this.preventClose) {
-			this.setState({
-				dialogOpen: !this.state.dialogOpen,
-			});
-		}
-	}
+  render() {
+    const props = this.props;
+    let dialog = this.state.dialogOpen ? (
+      <EnrollmentPopup
+        group={props.group}
+        hasChosen={props.hasChosen}
+        handlePopup={this.handlePopup}
+        hasChosenDay={props.hasChosenDay}
+        chosenDayGroupName={props.chosenDayGroupName}
+        preventPopupClose={this.preventPopupClose}
+      />
+    ) : null;
 
-	render() {
-		const props = this.props;
-		let dialog = this.state.dialogOpen ?
-			<EnrollmentPopup
-				group={props.group}
-				hasChosen={props.hasChosen}
-				handlePopup={this.handlePopup.bind(this)}
-				hasChosenDay={props.hasChosenDay}
-				chosenDayGroupName={props.chosenDayGroupName}
-				preventPopupClose={this.preventPopupClose.bind(this)}
-			/> : null;
+    if (props.loading) {
+      return <Progress size={30} />;
+    }
 
-		if (props.loading) {
-			return (
-				<Progress size={30} />
-			)
-		}
+    if (props.hasChosen) {
+      return (
+        <Button
+          color="primary"
+          onClick={() => this.handlePopup(false)}
+          style={props.style}
+        >
+          Ingeschreven
+          <Clear />
+          {dialog}
+        </Button>
+      );
+    }
 
-		if (props.hasChosen) {
-			return (
-				<Button color="primary" onClick={() => this.handlePopup(false)} style={props.style}>
-					{"Ingeschreven"}
-					<Clear />
-					{dialog}
-				</Button>
-			);
-		}
-
-		if (this.props.canChoose) {
-			return (
-				<Button color="primary" variant="contained" onClick={() => this.handlePopup(false)} style={this.props.style}>
-					{"Inschrijven" + (this.props.hasChosenDay ? "*" : "")}
-					{dialog}
-				</Button>
-			);
-		} else if (props.group.period < this.props.enrollmentPeriod) {
-			return (
-				<Button disabled color="primary" style={props.style}>
-					Inschrijfperiode verlopen
-					{dialog}
-				</Button>
-			);
-		} else {
-			return (
-				<Button disabled color="primary" style={props.style}>
-					Coming Soon
-					{dialog}
-				</Button>
-			);
-		}
-
-	}
+    if (this.props.canChoose) {
+      return (
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={() => this.handlePopup(false)}
+          style={this.props.style}
+        >
+          {"Inschrijven" + (this.props.hasChosenDay ? "*" : "")}
+          {dialog}
+        </Button>
+      );
+    } else if (props.group.period < this.props.enrollmentPeriod) {
+      return (
+        <Button disabled color="primary" style={props.style}>
+          Inschrijfperiode verlopen
+          {dialog}
+        </Button>
+      );
+    } else {
+      return (
+        <Button disabled color="primary" style={props.style}>
+          Coming Soon
+          {dialog}
+        </Button>
+      );
+    }
+  }
 }
 
 function mapStateToProps(state, ownProps) {
-	if (state.users[state.userId].enrollmentIds == null || state.enrollableGroups == null) {
-		return {
-			loading: true,
-		}
-	}
+  if (
+    state.users[state.userId].enrollmentIds == null ||
+    state.enrollableGroups == null
+  ) {
+    return {
+      loading: true
+    };
+  }
 
-	let chosenDayGroupName = -1;
-	state.users[state.userId].enrollmentIds.forEach(groupId => {
-		const group = state.groups[groupId];
-		if (group.day === ownProps.group.day && group.period === ownProps.group.period) {
-			chosenDayGroupName = group.courseName;
-		}
-	});
+  let chosenDayGroupName = -1;
+  state.users[state.userId].enrollmentIds.forEach(groupId => {
+    const group = state.groups[groupId];
+    if (
+      group.day === ownProps.group.day &&
+      group.period === ownProps.group.period
+    ) {
+      chosenDayGroupName = group.courseName;
+    }
+  });
 
-	return {
-		canChoose: state.enrollableGroups.map(e => e.id).indexOf(ownProps.group.id) !== -1,
-		hasChosen: state.users[state.userId].enrollmentIds.indexOf(ownProps.group.id) !== -1,
-		hasChosenDay: chosenDayGroupName !== -1,
-		chosenDayGroupName,
-		enrollmentPeriod: state.enrollmentPeriod,
-	};
+  return {
+    canChoose:
+      state.enrollableGroups.map(e => e.id).indexOf(ownProps.group.id) !== -1,
+    hasChosen:
+      state.users[state.userId].enrollmentIds.indexOf(ownProps.group.id) !== -1,
+    hasChosenDay: chosenDayGroupName !== -1,
+    chosenDayGroupName,
+    enrollmentPeriod: state.enrollmentPeriod
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		toggleEnrollment: (group) => dispatch(toggleEnrollment(group)),
-		getEnrollableGroups: () => dispatch(getEnrollableGroups()),
-		getEnrolLments: () => dispatch(getEnrolLments()),
-	};
+  return {
+    toggleEnrollment: group => dispatch(toggleEnrollment(group)),
+    getEnrollableGroups: () => dispatch(getEnrollableGroups()),
+    getEnrolLments: () => dispatch(getEnrolLments())
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChooseButton);
--- app/src/pages/group/EnrollmentPopup.js
+++ app/src/pages/group/EnrollmentPopup.js
@@ -1,152 +1,147 @@
-import React, { Component } from 'react';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Clear from '@material-ui/icons/Clear';
+import React, { Component } from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+  Button
+} from "@material-ui/core/";
+import Clear from "@material-ui/icons/Clear";
 
 class EnrollmentPopup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+  static getDerivedStateFromProps(nextProps, prevProps) {
+    return { page: nextProps.hasChosen ? "cancel" : "remarks", ...prevProps };
+  }
 
-	static getDerivedStateFromProps(nextProps, prevState) {
-		if (nextProps.hasChosen) {
-			return { page: "cancel", ...prevState };
-		}
-		if (nextProps.hasChosenDay) {
-			return { page: "hasChosenDay", ...prevState };
-		}
-		if (nextProps.group.remarks != null && nextProps.group.remarks.length > 1) {
-			return { page: "remarks", ...prevState };
-		}
-		return { page: "confirm", ...prevState };
-	}
+  getDuplicatePage() {
+    return (
+      <Dialog open keepMounted>
+        <DialogContent>
+          <Typography color="error" style={{ fontSize: "16px" }}>
+            {"Je hebt al een module gekozen voor " + this.props.group.day + "!"}
+          </Typography>
+          Je hebt je op deze dag al voor de module&nbsp;
+          <Typography color="primary" variant="button">
+            {this.props.chosenDayGroupName}
+          </Typography>
+          &nbsp;ingeschreven. Wil je je ook voor&nbsp;
+          <Typography color="primary" variant="button">
+            {this.props.group.courseName}
+          </Typography>
+          &nbsp;inschrijven?
+        </DialogContent>
+        <DialogActions>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={() => {
+              this.props.handlePopup(true);
+            }}
+          >
+            Toch inschrijven
+          </Button>
+          <Button color="default">Annuleren</Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
 
-	getDuplicatePage() {
-		return (
-			<Dialog open keepMounted >
-				<DialogContent>
-					<DialogContentText>
-						<Typography color="error" style={{ fontSize: "16px" }}>
-							{"Je hebt al een module gekozen voor " + this.props.group.day + "!"}
-						</Typography>
-						Schrijf je eerst uit voor de module
-						<Typography color="primary" variant="title" style={{ fontSize: "16px", display: "inline-block", margin: "5px" }}>
-							{this.props.chosenDayGroupName}
-						</Typography>
-						om je in te kunnen schrijven
-					</DialogContentText>
-				</DialogContent>
-				<DialogActions>
-					<Button color="primary" variant="contained">
-						Sluiten
-					</Button>
-				</DialogActions>
-			</Dialog>
-		);
-	}
+  getCancelPage() {
+    return (
+      <Dialog open keepMounted>
+        <DialogTitle>Uitschrijven</DialogTitle>
+        <DialogContent>
+          Weet je zeker dat je wilt uitschrijven voor de module&nbsp;
+          <Typography color="primary" variant="button">
+            {this.props.group.courseName}
+          </Typography>{" "}
+          &nbsp;?
+        </DialogContent>
+        <DialogActions>
+          <Button color="default">Annuleren</Button>
+          <Button onClick={() => this.props.handlePopup(true)} color="primary">
+            Uitschrijven
+            <Clear />
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
 
-	getCancelPage() {
-		return (
-			<Dialog open keepMounted >
-				<DialogTitle>
-					Uitschrijven
-				</DialogTitle>
-				<DialogContent>
-					<DialogContentText>
-						Weet je zeker dat je wilt uitschrijven voor de module
-						<Typography color="primary" variant="title" style={{ fontSize: "16px", display: "inline", margin: "5px" }}>
-							{this.props.group.courseName}
-						</Typography> ?
-					</DialogContentText>
-				</DialogContent>
-				<DialogActions>
-					<Button color="default">
-						Annuleren
-					</Button>
-					<Button name="test" onClick={() => this.props.handlePopup(true)} color="primary">
-						Uitschrijven
-						<Clear />
-					</Button>
-				</DialogActions>
-			</Dialog>
-		);
-	}
+  getConfirmPage() {
+    return (
+      <Dialog open keepMounted>
+        <DialogTitle>Inschrijven</DialogTitle>
+        <DialogContent>
+          Weet je zeker dat je wilt inschrijven voor de module&nbsp;
+          <Typography color="primary" variant="button">
+            {this.props.group.courseName}
+          </Typography>
+          &nbsp;?
+        </DialogContent>
+        <DialogActions>
+          <Button color="default">Annuleren</Button>
+          <Button
+            onClick={() => {
+              this.props.handlePopup(true);
+            }}
+            variant="contained"
+            color="primary"
+          >
+            Inschrijven
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
 
-	getConfirmPage() {
-		return (
-			<Dialog open keepMounted >
-				<DialogTitle>
-					Inschrijven
-				</DialogTitle>
-				<DialogContent>
-					<DialogContentText>
-						Weet je zeker dat je wilt inschrijven voor de module
-						<Typography color="primary" variant="title" style={{ fontSize: "16px", display: "inline", margin: "5px" }}>
-							{this.props.group.courseName}
-						</Typography> ?
-					</DialogContentText>
-				</DialogContent>
-				<DialogActions>
-					<Button color="default">
-						Annuleren
-					</Button>
-					<Button name="test" onClick={() => this.props.handlePopup(true)} variant="contained" color="primary">
-						Inschrijven
-					</Button>
-				</DialogActions>
-			</Dialog>
-		);
-	}
+  getRemarkPage = () => {
+    const remarks = this.props.group.remarks;
+    return (
+      <Dialog open keepMounted>
+        <DialogTitle>Opmerkingen</DialogTitle>
+        <DialogContent>
+          <Typography>{remarks}</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button color="default">Annuleren</Button>
+          <Button
+            onClick={() => {
+              this.props.preventPopupClose(true);
+              this.setState({ page: "hasChosenDay" });
+            }}
+            color="primary"
+          >
+            Verder
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
 
-	getRemarkPage() {
-		return (
-			<Dialog open keepMounted >
-				<DialogTitle>
-					Opmerkingen
-				</DialogTitle>
-				<DialogContent>
-					<Typography>
-						{this.props.group.remarks}
-					</Typography>
-				</DialogContent>
-				<DialogActions>
-					<Button color="default">
-						Annuleren
-					</Button>
-					<Button onClick={() => {
-						this.props.preventPopupClose(true);
-						this.setState({ page: "confirm" })
-					}} color="primary">
-						Verder
-					</Button>
-				</DialogActions>
-			</Dialog>
-		);
-	}
+  render() {
+    this.props.preventPopupClose(false);
+    const page = this.state.page;
 
-	render() {
-		this.props.preventPopupClose(false);
-		switch (this.state.page) {
-			case "remarks":
-				return this.getRemarkPage();
-			case "cancel":
-				return this.getCancelPage();
-			case "hasChosenDay":
-				return this.getDuplicatePage();
-			case "confirm":
-			default:
-				return this.getConfirmPage();
-		}
-	}
+    if (page === "cancel") return this.getCancelPage();
+    if (
+      page === "remarks" &&
+      this.props.group.remarks != null &&
+      this.props.group.remarks.length > 1
+    ) {
+      return this.getRemarkPage();
+    }
+    if (page === "hasChosenDay" && this.props.hasChosenDay) {
+      return this.getDuplicatePage();
+    }
+    return this.getConfirmPage();
+  }
 }
 
-
 export default EnrollmentPopup;
-
