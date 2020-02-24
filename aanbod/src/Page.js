47b5b35b4bc60060bf11cc2f5ec5af2be3ac18ef
StevenBrons <smp15.programming@gmail.com>
2020-02-24 14:38:29 +0100

    WIP

--- aanbod/src/Page.js
+++ aanbod/src/Page.js
@@ -4,7 +4,7 @@ import CourseInfo from './components/CourseInfo';
 import CourseGroup from './components/CourseGroup';
 import SubjectInfo from './components/SubjectInfo';
 // import Header from './components/Header';
-import {fetchCourses, fetchSubjectInformation} from './fetchData';
+import { fetchCourses, fetchSubjectInformation } from './fetchData';
 import LoadIcon from './components/LoadIcon';
 
 
@@ -22,11 +22,11 @@ class Page extends Component {
   }
 
   componentDidMount = () => {
-    fetchCourses(this.handleError).then(courses => 
-       this.setState({courses: courses, serverError: false}))
+    fetchCourses(this.handleError).then(courses =>
+      this.setState({ courses: courses, serverError: false }))
 
     fetchSubjectInformation(this.handleError).then(subjectDescriptions =>
-       this.setState({subjectDescriptions: subjectDescriptions, serverError: false}))
+      this.setState({ subjectDescriptions: subjectDescriptions, serverError: false }))
   }
 
   handleError = e => {
@@ -68,17 +68,17 @@ class Page extends Component {
   }
 
   render() {
-    const {popOut, courses, subjectInfo, subjectDescriptions, serverError} = this.state;
+    let { popOut, courses, subjectInfo, subjectDescriptions, serverError } = this.state;
     if (serverError) {
-      return(
+      return (
         <h1 className='error'>
           Er is een fout opgetreden met het verbinden met de server.
         </h1>
       )
     }
     if (!courses) {
-      return(
-        <LoadIcon/>
+      return (
+        <LoadIcon />
       )
     }
     const subjects = Object.keys(courses);
@@ -86,7 +86,7 @@ class Page extends Component {
     if (subjectInfo) {
       let subjectInfoId = subjects.indexOf(subjectInfo)
       if (subjectInfoId < subjects.length - 1) nextSubject = () => this.showSubjectInfo(subjects[subjectInfoId + 1]);
-      if (subjectInfoId > 0 ) previousSubject = () => this.showSubjectInfo(subjects[subjectInfoId - 1]);
+      if (subjectInfoId > 0) previousSubject = () => this.showSubjectInfo(subjects[subjectInfoId - 1]);
     }
     return (
       <div className='Page'>
@@ -100,24 +100,24 @@ class Page extends Component {
             onClose={() => this.showSubjectInfo(null)}
           />
         }
-        {subjects.map((subject,i) =>
+        {subjects.map((subject, i) =>
           <React.Fragment key={i}>
-            <CourseGroup 
-              title={subject} 
+            <CourseGroup
+              title={subject}
               key={i}
               courses={courses[subject]}
-              onClick={courseId => this.onClick(courseId,subject)} 
-              className='course-group' 
-              selectedCourse = {popOut && popOut.courseId}
+              onClick={courseId => this.onClick(courseId, subject)}
+              className='course-group'
+              selectedCourse={popOut && popOut.courseId}
               showSubjectInfo={() => this.showSubjectInfo(subject)}
             />
-            {popOut && popOut.group === subject && 
-              <CourseInfo 
-                course={courses[popOut.group][popOut.courseId]} 
+            {popOut && popOut.group === subject &&
+              <CourseInfo
+                course={courses[popOut.group][popOut.courseId]}
                 key={Math.random()}
-                group={subject} 
-                onClose={_ => this.onClick(popOut.courseId, popOut.group)} 
-              />  
+                group={subject}
+                onClose={_ => this.onClick(popOut.courseId, popOut.group)}
+              />
             }
           </React.Fragment>
         )}
