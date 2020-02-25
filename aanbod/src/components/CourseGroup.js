2826653117d017a7879812f6e7cad42a43cb9f25
jorritboer <jorritdeboer@MBP-van-Jorrit.lan>
2020-02-25 17:55:39 +0100

    Add mouse support for 2 courses horizontally

--- aanbod/src/components/CourseGroup.js
+++ aanbod/src/components/CourseGroup.js
@@ -7,24 +7,46 @@ class CourseGroup extends Component {
     super(props);
     this.scroller = React.createRef();
     this.title = React.createRef();
-    let maxPage = Math.floor((Object.keys(props.courses).length - 1) / 4);
+    let coursesPerPage = window.innerWidth <= 700 ? 2 : 4 ; // mobile : desktop
+    let maxPage = Math.floor((Object.keys(props.courses).length - 1) / coursesPerPage);
     this.state = {
       page: 0,
-      maxPage: maxPage
+      maxPage: maxPage,
+      coursesPerPage: coursesPerPage,
     };
-    if (maxPage > 0)
-      window.addEventListener("resize", (e) => {
-        // in case on mobile, we shouldn't readjust the pages
-        if (!(window.matchMedia('(pointer: coarse)').matches)){
-          this.scrollToPage(this.state.page);
-        } 
-      });
+    window.addEventListener("resize", (e) => {
+      // in case on mobile, we shouldn't readjust the pages
+      console.log(e)
+      if (!(window.matchMedia('(pointer: coarse)').matches)){
+        // Check if resize changed the amount of courses displayed per page 
+        if (window.innerWidth > 700 && this.state.coursesPerPage === 2) {
+          let page = Math.floor(this.state.page / 2);
+          let coursesPerPage = 4;
+          let maxPage = Math.floor((Object.keys(this.props.courses).length - 1) / coursesPerPage);
+          this.setState({
+            page: page,
+            coursesPerPage: coursesPerPage,
+            maxPage: maxPage,
+          })
+        } else if (window.innerWidth <= 700 && this.state.coursesPerPage === 4){
+          let page = this.state.page * 2;
+          let coursesPerPage = 2;
+          let maxPage = Math.floor((Object.keys(this.props.courses).length - 1) / coursesPerPage);
+          this.setState({
+            page: page,
+            coursesPerPage: coursesPerPage,
+            maxPage: maxPage,
+          })
+        }
+        this.scrollToPage(this.state.page);
+      } 
+    });
   }
 
   scrollToPage(page) {
     let scroller = this.scroller.current;
     const margin = this.title.current.offsetLeft;
-    scroller.scrollLeft = scroller.children[4 * page].offsetLeft - margin;
+    scroller.scrollLeft = scroller.children[this.state.coursesPerPage * page].offsetLeft - margin;
     this.setState({
       page: page
     });
