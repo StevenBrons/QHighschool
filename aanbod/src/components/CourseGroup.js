29c09ef474abd5305b95376e989befc6203f332d
StevenBrons <smp15.programming@gmail.com>
2020-03-10 12:26:14 +0100

    Fix thumbnails

--- aanbod/src/components/CourseGroup.js
+++ aanbod/src/components/CourseGroup.js
@@ -17,7 +17,7 @@ class CourseGroup extends Component {
     super(props);
     this.scroller = React.createRef();
     this.title = React.createRef();
-    let coursesPerPage = window.innerWidth <= 700 ? 2 : 4 ; // mobile : desktop
+    let coursesPerPage = window.innerWidth <= 700 ? 2 : 4; // mobile : desktop
     let maxPage = Math.floor((Object.keys(props.courses).length - 1) / coursesPerPage);
     let colors = this.randomColors(Object.keys(props.courses).length)
     this.state = {
@@ -29,7 +29,7 @@ class CourseGroup extends Component {
 
     window.addEventListener("resize", (e) => {
       // in case on mobile, we shouldn't readjust the pages
-      if (!(window.matchMedia('(pointer: coarse)').matches)){
+      if (!(window.matchMedia('(pointer: coarse)').matches)) {
         // Check if resize changed the amount of courses displayed per page 
         if (window.innerWidth > 700 && this.state.coursesPerPage === 2) {
           let page = Math.floor(this.state.page / 2);
@@ -40,7 +40,7 @@ class CourseGroup extends Component {
             coursesPerPage: coursesPerPage,
             maxPage: maxPage,
           })
-        } else if (window.innerWidth <= 700 && this.state.coursesPerPage === 4){
+        } else if (window.innerWidth <= 700 && this.state.coursesPerPage === 4) {
           let page = this.state.page * 2;
           let coursesPerPage = 2;
           let maxPage = Math.floor((Object.keys(this.props.courses).length - 1) / coursesPerPage);
@@ -51,7 +51,7 @@ class CourseGroup extends Component {
           })
         }
         this.scrollToPage(this.state.page);
-      } 
+      }
     });
   }
 
@@ -59,7 +59,7 @@ class CourseGroup extends Component {
     let res = [COLORS[Math.floor(Math.random() * 7)]]
     for (let i = 1; i < n; i++) {
       let colors = COLORS.slice()
-      colors.splice(colors.indexOf(res[res.length - 1]),1) // remove last color from pool
+      colors.splice(colors.indexOf(res[res.length - 1]), 1) // remove last color from pool
       res.push(colors[Math.floor(Math.random() * 6)])
     }
     return res
@@ -68,14 +68,14 @@ class CourseGroup extends Component {
   scrollToPage(page) {
     let scroller = this.scroller.current;
     const margin = this.title.current.offsetLeft;
-    scroller.scrollTo({left: scroller.children[this.state.coursesPerPage * page].offsetLeft - margin, behavior: 'smooth' });
+    scroller.scrollTo({ left: scroller.children[this.state.coursesPerPage * page].offsetLeft - margin, behavior: 'smooth' });
     this.setState({
       page: page
     });
   }
 
   getSortedCourseIds(courses) {// course objects -> ordered course ids
-    let ids = Object.keys(courses).sort((a,b) => {
+    let ids = Object.keys(courses).sort((a, b) => {
       let startYear = new RegExp(/(\d{4})\//) // '2019/2020' => 2019
       let yearA = parseInt(courses[a].schoolYear.match(startYear)[1])
       let yearB = parseInt(courses[b].schoolYear.match(startYear)[1])
@@ -109,11 +109,12 @@ class CourseGroup extends Component {
           />
         )}
         <div className="scroller" ref={this.scroller}>
-          {this.getSortedCourseIds(courses).map((id,i) => {
+          {this.getSortedCourseIds(courses).map((id, i) => {
             return (
               <Course
                 key={id}
-                courseId={id}
+                groupId={id}
+                courseId={courses[id].courseId}
                 class="course"
                 onClick={_ => this.props.onClick(id)}
                 text={courses[id].courseName.toUpperCase()}
