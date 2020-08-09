276661628a948f7d8d49ac616694bb055533ca5a
StevenBrons <smp15.programming@gmail.com>
2020-08-09 10:33:42 +0200

    Add schedule

--- api/lib/schedule.js
+++ api/lib/schedule.js
@@ -312,273 +312,261 @@ exports.schedule = [
     schoolYear: "2018/2019"
   },
 
-  //PERIOD 1 2019/2020
-  {
-    weekNumber: 35,
-    period: 1,
-    numberInBlock: 1,
-    year: 2019,
-    schoolYear: "2019/2020"
-  },
+  //PERIOD 1 2020/2021
   {
     weekNumber: 36,
     period: 1,
-    numberInBlock: 2,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 1,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 37,
     period: 1,
-    numberInBlock: 3,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 2,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 38,
     period: 1,
-    numberInBlock: 4,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 3,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 39,
     period: 1,
-    numberInBlock: 5,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 4,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 40,
     period: 1,
-    numberInBlock: 6,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 5,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 41,
     period: 1,
-    numberInBlock: 7,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 6,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
   {
-    weekNumber: 43,
+    weekNumber: 42,
     period: 1,
-    numberInBlock: 8,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 7,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 44,
     period: 1,
-    numberInBlock: 9,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 8,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
-  //PERIOD 2 2019/2020
   {
     weekNumber: 45,
-    period: 2,
-    numberInBlock: 1,
-    year: 2019,
-    schoolYear: "2019/2020"
+    period: 1,
+    numberInBlock: 9,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
+  //PERIOD 2 2020/2021
   {
     weekNumber: 46,
     period: 2,
-    numberInBlock: 2,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 1,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 47,
     period: 2,
-    numberInBlock: 3,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 2,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 48,
     period: 2,
-    numberInBlock: 4,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 3,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 49,
     period: 2,
-    numberInBlock: 5,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 4,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 50,
     period: 2,
-    numberInBlock: 6,
-    year: 2019,
-    schoolYear: "2019/2020"
+    numberInBlock: 5,
+    year: 2020,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 51,
     period: 2,
+    numberInBlock: 6,
+    year: 2020,
+    schoolYear: "2020/2021"
+  },
+  {
+    weekNumber: 1,
+    period: 2,
     numberInBlock: 7,
-    year: 2019,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 2,
     period: 2,
     numberInBlock: 8,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 3,
     period: 2,
     numberInBlock: 9,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
-  //PERIOD 3 2019/2020
+  //PERIOD 3 2020/2021
   {
     weekNumber: 4,
     period: 3,
     numberInBlock: 1,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 5,
     period: 3,
     numberInBlock: 2,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 6,
     period: 3,
     numberInBlock: 3,
-    year: 2020,
-    schoolYear: "2019/2020"
-  },
-  {
-    weekNumber: 7,
-    period: 3,
-    numberInBlock: 4,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 8,
     period: 3,
+    numberInBlock: 4,
+    year: 2021,
+    schoolYear: "2020/2021"
+  },
+  {
+    weekNumber: 9,
+    period: 3,
     numberInBlock: 5,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 10,
     period: 3,
     numberInBlock: 6,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 11,
     period: 3,
     numberInBlock: 7,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 12,
     period: 3,
     numberInBlock: 8,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 13,
     period: 3,
     numberInBlock: 9,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
-  //PERIOD 4 2019/2020
+  //PERIOD 4 2020/2021
   {
     weekNumber: 14,
     period: 4,
     numberInBlock: 1,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 15,
     period: 4,
     numberInBlock: 2,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 16,
     period: 4,
     numberInBlock: 3,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
+    weekNumber: 17,
     period: 4,
     numberInBlock: 4,
-    year: 2020,
-    schoolYear: "2019/2020",
-    weekNumber: (day) => {
-      switch (day) {
-        case "maandag":
-          return 17;
-        case "dinsdag":
-          return 17;
-        case "woensdag":
-          return 19;
-        case "donderdag":
-          return 19;
-        case "vrijdag":
-          return 19;
-      }
-    }
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 20,
     period: 4,
     numberInBlock: 5,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 21,
     period: 4,
     numberInBlock: 6,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 22,
     period: 4,
     numberInBlock: 7,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 23,
     period: 4,
     numberInBlock: 8,
-    year: 2020,
-    schoolYear: "2019/2020"
+    year: 2021,
+    schoolYear: "2020/2021"
   },
   {
     weekNumber: 24,
     period: 4,
     numberInBlock: 9,
-    year: 2020,
-    schoolYear: "2019/2020"
-  }
+    year: 2021,
+    schoolYear: "2020/2021"
+  },
+
 ];
